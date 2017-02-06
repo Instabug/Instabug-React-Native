d600b559b358c98a22e19c7523eec7c96d796f5e
Yousef Hamza <jo.adam.93@gmail.com>
2017-02-06 16:08:40 +0200

    Fix typo fixed in instabug 7.0

--- ios/RNInstabug/InstabugReactBridge.m
+++ ios/RNInstabug/InstabugReactBridge.m
@@ -78,14 +78,14 @@ RCT_EXPORT_METHOD(setPreInvocationHandler:(RCTResponseSenderBlock)callBack) {
 
 RCT_EXPORT_METHOD(setPostInvocationHandler:(RCTResponseSenderBlock)callBack) {
     if (callBack != nil) {
-        [Instabug setPostInvocatioHandler:^(IBGDismissType dismissType, IBGReportType reportType) {
+        [Instabug setPostInvocationHandler:^(IBGDismissType dismissType, IBGReportType reportType) {
             [self sendEventWithName:@"IBGpostInvocationHandler" body:@{
                                                                        @"dismissType": @(dismissType),
                                                                        @"reportType": @(reportType)
                                                                        }];
         }];
     } else {
-        [Instabug setPostInvocatioHandler:nil];
+        [Instabug setPostInvocationHandler:nil];
     }
 }
 
