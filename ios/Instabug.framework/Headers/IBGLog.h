369cabc9c885ea354e9ecb6ca14ce2b91e9d4654
Salma Ali <bluet.salma@gmail.com>
2019-06-12 09:39:30 +0200

    Release/8.4 (#301)
    * ⬆️ update native android SDK to version 8.4.0.1
    
    * 📝 change android private views native implementation to match ios
    
    * ⬆️ Update native iOS SDK to 8.4
    
    * 💎 Bump version to 8.4.0

--- ios/Instabug.framework/Headers/IBGLog.h
+++ ios/Instabug.framework/Headers/IBGLog.h
@@ -1,10 +1,12 @@
-//
-//  IBGLog.h
-//  InstabugI
-//
-//  Created by Yousef Hamza on 5/17/18.
-//  Copyright © 2018 Moataz. All rights reserved.
-//
+/*
+ File:       Instabug/IBGLog.h
+ 
+ Contains:   API for using Instabug's SDK.
+ 
+ Copyright:  (c) 2013-2019 by Instabug, Inc., all rights reserved.
+ 
+ Version:    8.4
+ */
 
 #import <Foundation/Foundation.h>
 #import "InstabugCore.h"
