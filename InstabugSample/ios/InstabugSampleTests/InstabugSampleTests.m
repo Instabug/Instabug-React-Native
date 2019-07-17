df3c5675df7c2ef63712e06b9cc4aa22bb44596d
Aly ezz <alyezz94@gmail.com>
2019-07-17 15:34:53 +0200

    ✨ ios native tests

--- InstabugSample/ios/InstabugSampleTests/InstabugSampleTests.m
+++ InstabugSample/ios/InstabugSampleTests/InstabugSampleTests.m
@@ -31,4 +31,76 @@
   [[[mock verify] classMethod] showSurveyWithToken:token];
 }
 
+
+/***********Bug Reporting*****************/
+
+- (void) testgivenBoolean$setBugReportingEnabled_whenQuery_thenShouldCallNativeApi {
+  BOOL enabled = false;
+  [self.instabugBridge setBugReportingEnabled:enabled];
+  XCTAssertFalse(IBGBugReporting.enabled);
+}
+
+- (void) testgivenInvocationEvent$setInvocationEvents_whenQuery_thenShouldCallNativeApiWithArgs {
+  NSArray *invocationEventsArr;
+  invocationEventsArr = [NSArray arrayWithObjects:  @(IBGInvocationEventScreenshot), nil];
+  
+  [self.instabugBridge setInvocationEvents:invocationEventsArr];
+  IBGInvocationEvent invocationEvents = 0;
+  for (NSNumber *boxedValue in invocationEventsArr) {
+    invocationEvents |= [boxedValue intValue];
+  }
+  XCTAssertEqual(IBGBugReporting.invocationEvents, invocationEvents);
+}
+
+- (void)testgiven$invoke_whenQuery_thenShouldCallNativeApi {
+  id mock = OCMClassMock([IBGBugReporting class]);
+  
+  [[[mock stub] classMethod] invoke];
+  [self.instabugBridge invoke];
+  double delayInSeconds = 2.0;
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+     [[[mock verify] classMethod] invoke];
+  });
+}
+
+- (void) testgivenOptions$setInvocationOptions_whenQuery_thenShouldCallNativeApiWithArgs {
+  NSArray *invocationOptionsArr;
+  invocationOptionsArr = [NSArray arrayWithObjects:  @(IBGBugReportingInvocationOptionEmailFieldHidden), nil];
+  
+  [self.instabugBridge setInvocationOptions:invocationOptionsArr];
+  IBGBugReportingInvocationOption invocationOptions = 0;
+  for (NSNumber *boxedValue in invocationOptionsArr) {
+    invocationOptions |= [boxedValue intValue];
+  }
+  XCTAssertEqual(IBGBugReporting.invocationOptions, invocationOptions);
+}
+
+- (void) testgivenInvocationModeAndOptiond$invokeWithModeOptions_whenQuery_thenShouldCallNativeApiWithArgs {
+  NSArray *invocationOptionsArr;
+  invocationOptionsArr = [NSArray arrayWithObjects:  @(IBGBugReportingInvocationOptionEmailFieldHidden), nil];
+  IBGBugReportingInvocationOption invocationOptions = 0;
+  for (NSNumber *boxedValue in invocationOptionsArr) {
+    invocationOptions |= [boxedValue intValue];
+  }
+  IBGInvocationMode invocationMode = IBGInvocationModeNewBug;
+  id mock = OCMClassMock([IBGBugReporting class]);
+  
+  [[[mock stub] classMethod] invokeWithMode:invocationMode options:invocationOptions];
+  [self.instabugBridge invokeWithInvocationModeAndOptions:invocationMode options:invocationOptionsArr];
+  double delayInSeconds = 2.0;
+  dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
+  dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+    [[[mock verify] classMethod] invokeWithMode:invocationMode options:invocationOptions];
+  });
+}
+
+
+- (void) test {
+  [self.instabugBridge setPreInvocationHandler:nil];
+  XCTAssertEqual(IBGBugReporting.willInvokeHandler, nil);
+}
+
+
+
 @end
--- ios/RNInstabug/InstabugReactBridge.h
+++ ios/RNInstabug/InstabugReactBridge.h
@@ -9,9 +9,29 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#import <Instabug/Instabug.h>
+#import <Instabug/IBGBugReporting.h>
+#import <Instabug/IBGCrashReporting.h>
+#import <Instabug/IBGSurveys.h>
+#import <Instabug/IBGLog.h>
 
 @interface InstabugReactBridge : RCTEventEmitter <RCTBridgeModule>
 
 - (void)showSurveyWithToken:(NSString *)surveyToken;
 
+
+/***********Bug Reporting*****************/
+
+
+- (void)setBugReportingEnabled:(BOOL) isEnabled;
+
+- (void)setInvocationEvents:(NSArray*)invocationEventsArray;
+
+- (void)invoke;
+
+- (void)setInvocationOptions:(NSArray*)invocationOptionsArray;
+
+- (void)invokeWithInvocationModeAndOptions:(IBGInvocationMode)invocationMode options:(NSArray*)options;
+
+- (void)setPreInvocationHandler:(RCTResponseSenderBlock)callBack;
 @end
