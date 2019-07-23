23ed60389131653a5fa79264ee2613d63bc278a5
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2019-07-23 14:17:13 +0200

    Add Instabug module methods to InstabugReactBridge.h

--- ios/RNInstabug/InstabugReactBridge.h
+++ ios/RNInstabug/InstabugReactBridge.h
@@ -14,24 +14,86 @@
 #import <Instabug/IBGCrashReporting.h>
 #import <Instabug/IBGSurveys.h>
 #import <Instabug/IBGLog.h>
+#import <Instabug/IBGTypes.h>
 
 @interface InstabugReactBridge : RCTEventEmitter <RCTBridgeModule>
 
-- (void)showSurveyWithToken:(NSString *)surveyToken;
+/*
+ +------------------------------------------------------------------------+
+ |                            Instabug Module                             |
+ +------------------------------------------------------------------------+
+ */
 
+- (void)startWithToken:(NSString *)token invocationEvents:(NSArray *)invocationEventsArray;
+
+- (void)setUserData:(NSString *)userData;
+
+- (void)setTrackUserSteps:(BOOL)isEnabled;
+
+- (void)setSessionProfilerEnabled:(BOOL)sessionProfilerEnabled;
+
+- (void)setPushNotificationsEnabled:(BOOL)isPushNotificationEnabled;
+
+- (void)setLocale:(IBGLocale)locale;
+
+- (void)setColorTheme:(IBGColorTheme)colorTheme;
+
+- (void)setPrimaryColor:(UIColor *)color;
+
+- (void)appendTags:(NSArray *)tags;
+
+- (void)resetTags;
+
+- (void)getTags:(RCTResponseSenderBlock)callBack;
+
+- (void)setString:(NSString *)value toKey:(NSString *)key;
+
+- (void)identifyUserWithEmail:(NSString *)email name:(NSString *)name;
+
+- (void)logOut;
+
+- (void)logUserEventWithName:(NSString *)name;
+
+- (void)logVerbose:(NSString *)log;
+
+- (void)setReproStepsMode:(IBGUserStepsMode)reproStepsMode;
+
+- (void)setUserAttribute:(NSString *)key withValue:(NSString *)value;
+
+- (void)getUserAttribute:(NSString *)key callback:(RCTResponseSenderBlock)callback;
+
+- (void)removeUserAttribute:(NSString *)key;
+
+- (void)getAllUserAttributes:(RCTResponseSenderBlock)callback;
+
+- (void)clearAllUserAttributes;
+
+- (void)showWelcomeMessageWithMode:(IBGWelcomeMessageMode)welcomeMessageMode;
+
+- (void)setWelcomeMessageMode:(IBGWelcomeMessageMode)welcomeMessageMode;
+
+- (void)setFileAttachment:(NSString *)fileLocation;
+
+- (void)show;
+/*
+ +------------------------------------------------------------------------+
+ |                            Surveys Module                             |
+ +------------------------------------------------------------------------+
+ */
+
+- (void)showSurveyWithToken:(NSString *)surveyToken;
 
 /***********Bug Reporting*****************/
 
+- (void)setBugReportingEnabled:(BOOL)isEnabled;
 
-- (void)setBugReportingEnabled:(BOOL) isEnabled;
-
-- (void)setInvocationEvents:(NSArray*)invocationEventsArray;
+- (void)setInvocationEvents:(NSArray *)invocationEventsArray;
 
 - (void)invoke;
 
-- (void)setInvocationOptions:(NSArray*)invocationOptionsArray;
+- (void)setInvocationOptions:(NSArray *)invocationOptionsArray;
 
-- (void)invokeWithInvocationModeAndOptions:(IBGInvocationMode)invocationMode options:(NSArray*)options;
+- (void)invokeWithInvocationModeAndOptions:(IBGInvocationMode)invocationMode options:(NSArray *)options;
 
 - (void)setPreInvocationHandler:(RCTResponseSenderBlock)callBack;
 
@@ -45,14 +107,15 @@
 
 - (void)setExtendedBugReportMode:(IBGExtendedBugReportMode)extendedBugReportMode;
 
-- (void)setReportTypes:(NSArray*)types;
+- (void)setReportTypes:(NSArray *)types;
 
-- (void)showBugReportingWithReportTypeAndOptions:(IBGBugReportingReportType) type: (NSArray*) options;
+- (void)showBugReportingWithReportTypeAndOptions:(IBGBugReportingReportType)type
+                                                :(NSArray *)options;
 
-- (void)setAutoScreenRecordingEnabled:(BOOL) enabled;
+- (void)setAutoScreenRecordingEnabled:(BOOL)enabled;
 
-- (void)setAutoScreenRecordingMaxDuration:(CGFloat) duration;
+- (void)setAutoScreenRecordingMaxDuration:(CGFloat)duration;
 
-- (void)setViewHierarchyEnabled:(BOOL) viewHierarchyEnabled;
+- (void)setViewHierarchyEnabled:(BOOL)viewHierarchyEnabled;
 
 @end
