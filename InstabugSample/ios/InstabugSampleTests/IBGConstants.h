ebf72d47d48ccee08bef48bb63a6e2bae4b13c01
Salma Ali <bluet.salma@gmail.com>
2019-08-05 10:02:49 +0200

    🔨 Split native iOS tests based on module

---
+++ InstabugSample/ios/InstabugSampleTests/IBGConstants.h
@@ -0,0 +1,14 @@
+//
+//  IBGConstants.h
+//  InstabugSample
+//
+//  Created by Salma Ali on 7/30/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#ifndef IBGConstants_h
+#define IBGConstants_h
+
+extern NSTimeInterval const EXPECTATION_TIMEOUT;
+
+#endif /* IBGConstants_h */
