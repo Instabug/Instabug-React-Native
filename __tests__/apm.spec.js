eb2fa731729b31362c3d56703b1520908428a77d
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2021-02-10 12:40:29 +0200

    Clean up APM specs

--- __tests__/apm.spec.js
+++ __tests__/apm.spec.js
@@ -9,7 +9,6 @@ import "../jest/mockAPM";
 import APM from "../modules/APM";
 import sinon from "sinon";
 
-import IBGConstants from "../utils/InstabugConstants";
 import IBGEventEmitter from "../utils/IBGEventEmitter";
 
 describe("APM Module", () => {
@@ -24,8 +23,6 @@ describe("APM Module", () => {
   const endUITrace = sinon.spy(NativeModules.IBGAPM, "endUITrace");
 
   beforeEach(() => {
-    // setWillShowSurveyHandler.resetHistory();
-    // setDidDismissSurveyHandler.resetHistory();
     IBGEventEmitter.removeAllListeners();
   });
 
@@ -84,109 +81,4 @@ describe("APM Module", () => {
 
     expect(endUITrace.calledOnceWithExactly()).toBe(true);
   });
-    
-  // it("should call the native method setAppStoreURL", () => {
-  //   Surveys.setAppStoreURL("URL");
-
-  //   expect(setAppStoreURL.calledOnceWithExactly("URL")).toBe(true);
-  // });
-
-  // it("should call the native method showSurveysIfAvailable", () => {
-  //   Surveys.showSurveyIfAvailable();
-
-  //   expect(showSurveysIfAvailable.calledOnce).toBe(true);
-  // });
-
-  // it("should call the native method setThresholdForReshowingSurveyAfterDismiss", () => {
-  //   const sessionCount = 2;
-  //   const daysCount = 3;
-  //   Surveys.setThresholdForReshowingSurveyAfterDismiss(sessionCount, daysCount);
-
-  //   expect(
-  //     setThresholdForReshowingSurveyAfterDismiss.calledOnceWithExactly(
-  //       sessionCount,
-  //       daysCount
-  //     )
-  //   ).toBe(true);
-  // });
-
-  // it("should call the native method getAvailableSurveys", () => {
-  //   const callback = jest.fn();
-  //   Surveys.getAvailableSurveys(callback);
-
-  //   expect(getAvailableSurveys.calledOnceWithExactly(callback)).toBe(true);
-  // });
-
-  // it("should call the native method setAutoShowingSurveysEnabled", () => {
-  //   Surveys.setAutoShowingEnabled(true);
-
-  //   expect(setAutoShowingSurveysEnabled.calledOnceWithExactly(true)).toBe(true);
-  // });
-
-  // it("should call the native method setWillShowSurveyHandler with a function", () => {
-  //   const callback = jest.fn();
-  //   Surveys.setOnShowHandler(callback);
-
-  //   expect(setWillShowSurveyHandler.calledOnceWithExactly(callback)).toBe(true);
-  // });
-
-  // it("should invoke callback on emitting the event IBGWillShowSurvey", () => {
-  //   const callback = jest.fn();
-  //   Surveys.setOnShowHandler(callback);
-  //   IBGEventEmitter.emit(IBGConstants.WILL_SHOW_SURVEY_HANDLER);
-
-  //   expect(
-  //     IBGEventEmitter.getListeners(IBGConstants.WILL_SHOW_SURVEY_HANDLER).length
-  //   ).toEqual(1);
-  //   expect(callback).toHaveBeenCalled();
-  // });
-
-  // it("should call the native method setDidDismissSurveyHandler with a function", () => {
-  //   const callback = jest.fn();
-  //   Surveys.setOnDismissHandler(callback);
-
-  //   expect(setDidDismissSurveyHandler.calledOnceWithExactly(callback)).toBe(
-  //     true
-  //   );
-  // });
-
-  // it("should invoke callback on emitting the event IBGDidDismissSurvey", () => {
-  //   const callback = jest.fn();
-  //   Surveys.setOnDismissHandler(callback);
-  //   IBGEventEmitter.emit(IBGConstants.DID_DISMISS_SURVEY_HANDLER);
-
-  //   expect(
-  //     IBGEventEmitter.getListeners(IBGConstants.DID_DISMISS_SURVEY_HANDLER)
-  //       .length
-  //   ).toEqual(1);
-  //   expect(callback).toHaveBeenCalled();
-  // });
-
-  // it("should call the native method showSurveyWithToken", () => {
-  //   const surveyToken = "HEU128JD";
-  //   Surveys.showSurvey(surveyToken);
-
-  //   expect(showSurveyWithToken.calledOnceWithExactly(surveyToken)).toBe(true);
-  // });
-
-  // it("should call the native method hasRespondedToSurveyWithToken", done => {
-  //   const callback = hasResponded => {
-  //     expect(hasResponded).toBe(true);
-  //     done();
-  //   };
-  //   const surveyToken = "HEU128JD";
-  //   Surveys.hasRespondedToSurvey(surveyToken, callback);
-
-  //   expect(
-  //     hasRespondedToSurveyWithToken.calledOnceWithExactly(surveyToken, callback)
-  //   ).toBe(true);
-  // });
-
-  // it("should call the native method setShouldShowSurveysWelcomeScreen", () => {
-  //   Surveys.setShouldShowWelcomeScreen(true);
-
-  //   expect(setShouldShowSurveysWelcomeScreen.calledOnceWithExactly(true)).toBe(
-  //     true
-  //   );
-  // });
 });
