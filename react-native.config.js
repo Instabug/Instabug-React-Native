3b56b23a03de927190ad340443fef0959f3a6d85
Ali Abdelfattah <ali.abdelfattah.xa@gmail.com>
2021-04-19 00:01:51 +0200

    Remove unused linking files

--- react-native.config.js
+++ react-native.config.js
@@ -21,17 +21,13 @@ module.exports = {
     {
       name: 'add-instabug',
       func: () => {
-                    const exec = require('child_process').exec;
                     require('./link_gradle');
-                    exec("ruby ./node_modules/instabug-reactnative/autolink.rb || echo \"Ruby doesn't exist, if you're building this for Android only, then feel free to ignore this error, otherwise please install Ruby and run 'react-native link instabug-reactnative' again\"");
                   },
     },
     {
       name: 'remove-instabug',
       func: () => {
-                    const exec = require('child_process').exec;
                     require('./unlink_gradle');
-                    exec("ruby ./node_modules/instabug-reactnative/autounlink.rb || echo \"Ruby doesn't exist, if you're building this for Android only, then feel free to ignore this error, otherwise please install Ruby and run 'react-native link instabug-reactnative' again\"");
                   },
     },
   ],
