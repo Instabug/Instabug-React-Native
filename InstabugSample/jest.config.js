1564bd2b60a766c220ceb2beb838b39e5790e78e
shehabeldeenibrahim <shehabtibrahim@gmail.com>
2021-10-11 01:06:53 +0200

    fixed js tests

---
+++ InstabugSample/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'react-native',
+  moduleNameMapper: {
+    '^react-native$': require.resolve('react-native'),
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-navigation-tabs' +
+      '|react-native-splash-screen' +
+      '|react-native-screens' +
+      '|react-native-reanimated' +
+      '|instabug-reactnative' +
+      ')/)',
+  ],
+};
