36b54bc3cea5538313dd31b2fce128d5ebf5d9f6
shehabeldeenibrahim <shehabtibrahim@gmail.com>
2021-10-11 03:07:01 +0200

    fix detox test trial

--- InstabugSample/__e2e__/init.js
+++ InstabugSample/__e2e__/init.js
@@ -5,11 +5,11 @@ const specReporter = require('detox/runners/jest/specReporter');
 
 // Set the default timeout
 jest.setTimeout(120000);
-jasmine.getEnv().addReporter(adapter);
+// jasmine.getEnv().addReporter(adapter);
 
 // This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
 // This is strictly optional.
-jasmine.getEnv().addReporter(specReporter);
+// jasmine.getEnv().addReporter(specReporter);
 
 beforeAll(async () => {
   await detox.init(config);
