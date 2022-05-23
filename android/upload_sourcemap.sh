d57ff222ab9318dec864f86c4369b6096e6e226a
Ahmed Mahmoud <68241710+a7med-mahmoud@users.noreply.github.com>
2022-05-23 19:13:07 +0200

    [MOB-8853] Dynamic Package Path (#678)
    * Pass base dir from Gradle to upload_sourcemap.sh
    
    * Use absolute module import for parseErrorStackLib
    
    * Use absolute path for iOS upload_sourcemap.sh
    
    * Pass Android rootDir to upload_sourcemap as an arg
    
    * Use `rootDir` instead of `System.getProperty('user.dir')`.
    * Pass the path to user's Android directory (`rootDir`) as an argument to `upload_sourcemap.sh` instead of using environment variables
    * Remove the check for the user's Android directory path in `upload_sourcemap.sh` as it's passed from Gradle
    
    * Get `link_bridge.js` path dynamically on linking
    
    * Fix manual linking paths
    
    * Import `package.json` from relative file path instead of a module import
    * Dynamically get `link.rb` file path

--- android/upload_sourcemap.sh
+++ android/upload_sourcemap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-cd ..
-cd ..
-cd ..
+USER_ANDROID_DIR=$1
+cd "${USER_ANDROID_DIR}/.."
+
 if [ -s "$HOME/.nvm/nvm.sh" ]; then
 . "$HOME/.nvm/nvm.sh"
 elif [[ -x "$(command -v brew)" && -s "$(brew --prefix nvm)/nvm.sh" ]]; then
