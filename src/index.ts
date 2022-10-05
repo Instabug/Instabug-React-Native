f180dc61d7c8abd876fe3f7da826103b879125f8
Youssef Raafat <yraafat@instabug.com>
2022-10-05 14:32:09 +0200

    Export Models

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+// Models
+export { default as Report } from './models/Report';
+export { default as Trace } from './models/Trace';
+
+// Modules
 export { APM } from './modules/APM';
 export { BugReporting } from './modules/BugReporting';
 export { CrashReporting } from './modules/CrashReporting';
 export { FeatureRequests } from './modules/FeatureRequests';
+export { Instabug as default } from './modules/Instabug';
 export { NetworkLogger } from './modules/NetworkLogger';
 export { Replies } from './modules/Replies';
 export { Surveys, Survey } from './modules/Surveys';
-
-export { Instabug as default } from './modules/Instabug';
