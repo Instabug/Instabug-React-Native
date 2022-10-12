452d7dc618865852fa9f500f705870b12b311d04
Youssef Raafat <yraafat@instabug.com>
2022-10-12 12:26:30 +0200

    Remove ESLint from Example

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,24 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
   extends: '@react-native-community',
-  plugins: ['prettier'],
+  plugins: ['prettier', 'jest'],
+  env: {
+    node: true,
+    browser: true,
+    'jest/globals': true,
+  },
+  overrides: [
+    {
+      // Detox Overrides
+      files: ['example/e2e/**.js'],
+      globals: {
+        device: false,
+        waitFor: false,
+        element: false,
+        by: false,
+      },
+    },
+  ],
   rules: {
     'prettier/prettier': 'error',
   },
