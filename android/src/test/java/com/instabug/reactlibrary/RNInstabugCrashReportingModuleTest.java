19414a95d4a7131df81348e0d18befbd4cd5d7be
Youssef Raafat <yraafat@instabug.com>
2022-10-12 10:26:29 +0200

    Extract Android CR Unit Tests

---
+++ android/src/test/java/com/instabug/reactlibrary/RNInstabugCrashReportingModuleTest.java
@@ -0,0 +1,34 @@
+package com.instabug.reactlibrary;
+
+import org.junit.Test;
+
+
+public class RNInstabugCrashReportingModuleTest {
+    @Test
+    public void givenString$sendHandledJSCrash_whenQuery_thenShouldCallNativeApiWithArgs() throws Exception {
+//        JSONObject json = mock(JSONObject.class);
+//        PowerMockito.whenNew(JSONObject.class).withArguments("exception").thenReturn(json);
+//
+//        // given
+//        PowerMockito.mockStatic(CrashReporting.class);
+//        // when
+//        rnModule.sendHandledJSCrash("exception");
+//        // then
+//        JSONObject jsonObject = new JSONObject("exception");
+//        PowerMockito.verifyPrivate(CrashReporting.class, VerificationModeFactory.times(1)).invoke("reportException", jsonObject, true);
+    }
+
+    @Test
+    public void givenString$sendJSCrash_whenQuery_thenShouldCallNativeApiWithArgs() throws Exception {
+//        JSONObject json = mock(JSONObject.class);
+//        PowerMockito.whenNew(JSONObject.class).withArguments("exception").thenReturn(json);
+//
+//        // given
+//        PowerMockito.mockStatic(CrashReporting.class);
+//        // when
+//        rnModule.sendJSCrash("exception");
+//        // then
+//        JSONObject jsonObject = new JSONObject("exception");
+//        PowerMockito.verifyPrivate(CrashReporting.class, VerificationModeFactory.times(1)).invoke("reportException", jsonObject, false);
+    }
+}
--- android/src/test/java/com/instabug/reactlibrary/RNInstabugReactnativeModuleTest.java
+++ android/src/test/java/com/instabug/reactlibrary/RNInstabugReactnativeModuleTest.java
@@ -91,40 +91,6 @@ public class RNInstabugReactnativeModuleTest {
         mockInstabug.close();
     }
 
-
-    /********CrashReporting*********/
-
-//
-//    @Test
-//    public void givenString$sendHandledJSCrash_whenQuery_thenShouldCallNativeApiWithArgs() throws Exception {
-//
-//            JSONObject json = mock(JSONObject.class);
-//            PowerMockito.whenNew(JSONObject.class).withArguments("exception").thenReturn(json);
-//
-//            // given
-//            PowerMockito.mockStatic(CrashReporting.class);
-//            // when
-//            rnModule.sendHandledJSCrash("exception");
-//            // then
-//            JSONObject jsonObject = new JSONObject("exception");
-//            PowerMockito.verifyPrivate(CrashReporting.class, VerificationModeFactory.times(1)).invoke("reportException", jsonObject, true);
-//    }
-//
-//    @Test
-//    public void givenString$sendJSCrash_whenQuery_thenShouldCallNativeApiWithArgs() throws Exception {
-//
-//            JSONObject json = mock(JSONObject.class);
-//            PowerMockito.whenNew(JSONObject.class).withArguments("exception").thenReturn(json);
-//
-//            // given
-//            PowerMockito.mockStatic(CrashReporting.class);
-//            // when
-//            rnModule.sendJSCrash("exception");
-//            // then
-//            JSONObject jsonObject = new JSONObject("exception");
-//            PowerMockito.verifyPrivate(CrashReporting.class, VerificationModeFactory.times(1)).invoke("reportException", jsonObject, false);
-//    }
-
     /********Instabug*********/
 
     @Test
