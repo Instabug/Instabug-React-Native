1dee3c37bdb771ed1f9dc3c9e2dc042d75595432
DavidMina96 <david.mina96@hotmail.com>
2022-12-13 17:06:37 +0200

    Check reportJSException error type internally
    Enforcing a strong type for this API would require code changes from the
    users as the catch block parameter is of type any/unknown, so we handle
    the type internally.

--- src/modules/CrashReporting.ts
+++ src/modules/CrashReporting.ts
@@ -1,5 +1,4 @@
 import { Platform } from 'react-native';
-import type { ExtendedError } from 'react-native/Libraries/Core/Devtools/parseErrorStack';
 
 import { NativeCrashReporting } from '../native';
 import IBGEventEmitter from '../utils/IBGEventEmitter';
@@ -19,25 +18,27 @@ export const setEnabled = (isEnabled: boolean) => {
  * Send handled JS error object
  * @param error Error object to be sent to Instabug's servers
  */
-export const reportJSException = (error: ExtendedError) => {
-  const jsStackTrace = InstabugUtils.getStackTrace(error);
+export const reportJSException = (error: any) => {
+  if (error instanceof Error) {
+    const jsStackTrace = InstabugUtils.getStackTrace(error);
 
-  const jsonObject = {
-    message: error.name + ' - ' + error.message,
-    e_message: error.message,
-    e_name: error.name,
-    os: Platform.OS,
-    platform: 'react_native',
-    exception: jsStackTrace,
-  };
+    const jsonObject = {
+      message: error.name + ' - ' + error.message,
+      e_message: error.message,
+      e_name: error.name,
+      os: Platform.OS,
+      platform: 'react_native',
+      exception: jsStackTrace,
+    };
 
-  if (InstabugUtils.isOnReportHandlerSet() && Platform.OS === 'android') {
-    IBGEventEmitter.emit(InstabugConstants.SEND_HANDLED_CRASH, jsonObject);
-  } else {
-    if (Platform.OS === 'android') {
-      NativeCrashReporting.sendHandledJSCrash(JSON.stringify(jsonObject));
+    if (InstabugUtils.isOnReportHandlerSet() && Platform.OS === 'android') {
+      IBGEventEmitter.emit(InstabugConstants.SEND_HANDLED_CRASH, jsonObject);
     } else {
-      NativeCrashReporting.sendHandledJSCrash(jsonObject);
+      if (Platform.OS === 'android') {
+        NativeCrashReporting.sendHandledJSCrash(JSON.stringify(jsonObject));
+      } else {
+        NativeCrashReporting.sendHandledJSCrash(jsonObject);
+      }
     }
   }
 };
