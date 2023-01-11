1f58dbe8cc6336ff964d7f9c5c78232589eeff93
DavidMina96 <david.mina96@hotmail.com>
2023-01-11 15:12:57 +0200

    Enhance code style and readability

--- cli/UploadSourcemaps.ts
+++ cli/UploadSourcemaps.ts
@@ -4,7 +4,16 @@ import FormData from 'form-data';
 import fs from 'fs';
 import path from 'path';
 
-export const uploadSourcemapsCommand: Command = new Command();
+interface UploadSourcemapsOptions {
+  platform: 'android' | 'ios';
+  dir: string;
+  token: string;
+  name: string;
+  code: string;
+  label?: string;
+}
+
+export const uploadSourcemapsCommand = new Command();
 
 uploadSourcemapsCommand
   .name('upload-sourcemaps')
@@ -34,55 +43,50 @@ uploadSourcemapsCommand
       .env('INSTABUG_APP_VERSION_CODE')
       .makeOptionMandatory(),
   )
-  .addOption(new Option('-l, --label <value>', "The CodePush label if it's a CodePush release"))
+  .addOption(
+    new Option('-l, --label <value>', "The CodePush label if it's a CodePush release").env(
+      'INSTABUG_APP_VERSION_LABEL',
+    ),
+  )
   .action(function (this: Command) {
     const options = this.opts<UploadSourcemapsOptions>();
-    uploadFile(options);
+    uploadSourcemaps(options);
   })
   .showHelpAfterError();
 
-interface UploadSourcemapsOptions {
-  platform: 'android' | 'ios';
-  dir: string;
-  token: string;
-  name: string;
-  code: string;
-  label?: string;
-}
+const uploadSourcemaps = async (opts: UploadSourcemapsOptions) => {
+  const fileName = `${opts.platform}-sourcemap.json`;
+  const filePath = path.join(opts.dir, fileName);
+  const fileBlob = fs.readFileSync(filePath);
 
-const uploadFile = async (options: UploadSourcemapsOptions) => {
-  const os = options.platform;
-  const dir = options.dir;
-  const token = options.token;
-  const name = options.name;
-  const code = options.code;
-  const label = options.label;
   const version = {
-    code: code,
-    name: name,
-    codePush: label,
+    code: opts.code,
+    name: opts.name,
+    codePush: opts.label,
   };
+
   const form = new FormData();
   form.append('app_version', JSON.stringify(version));
-  form.append(
-    'symbols_file',
-    fs.readFileSync(path.join(dir, `${os}-sourcemap.json`)),
-    `${os}-sourcemap.json`,
-  );
-  form.append('application_token', token);
+  form.append('symbols_file', fileBlob, fileName);
+  form.append('application_token', opts.token);
   form.append('platform', 'react_native');
-  form.append('os', os);
+  form.append('os', opts.platform);
+
   console.log('Uploading Sourcemap file...');
   try {
     const response = await axios.post('https://api.instabug.com/api/sdk/v3/symbols_files', form, {
-      headers: {
-        ...form.getHeaders(),
-      },
+      headers: form.getHeaders(),
     });
-    console.log('Success:', response.data);
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error('Error:', error.response?.data);
+    console.log(response.data);
+    const appVersion = version.codePush
+      ? `${version.name} (${version.code})+codepush:${version.codePush}`
+      : `${version.name} (${version.code})`;
+    console.log(`Successfully uploaded Sourcemaps for version: ${appVersion}`);
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.error('Error:', err.response?.data);
+    } else {
+      console.error(err);
     }
   }
 };
