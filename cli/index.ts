f0c086163754de997f53006e30c5e7fc497ae180
DavidMina96 <david.mina96@hotmail.com>
2023-01-12 01:22:13 +0200

    Remove `figlet`

--- cli/index.ts
+++ cli/index.ts
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import figlet from 'figlet';
 
 import { uploadSourcemapsCommand } from './UploadSourcemaps';
 
 const program = new Command();
-
-console.log(figlet.textSync('Instabug CLI'));
 
 program
   .name('instaubg')
