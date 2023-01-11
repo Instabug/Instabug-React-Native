74db1f61c4fa377706f94acabf7df4817436c0d4
DavidMina96 <david.mina96@hotmail.com>
2023-01-12 00:50:03 +0200

    Update `rollup` and `cli` config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,20 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import shebang from 'rollup-plugin-preserve-shebang';
 
 export default {
-  input: ['bin/index.js'],
+  input: ['cli/index.ts'],
   output: {
     dir: 'bin',
     format: 'cjs',
   },
-  plugins: [shebang(), nodeResolve({ preferBuiltins: true }), commonjs(), json()],
+  plugins: [
+    typescript({ tsconfig: './tsconfig.cli.json' }),
+    shebang(),
+    json(),
+    nodeResolve({ preferBuiltins: true }),
+    commonjs(),
+  ],
 };
