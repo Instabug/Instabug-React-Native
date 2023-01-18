a1a2588ad59ba5d7934cdfcb7d11f1095b6e5a37
Youssef Raafat <yraafat@instabug.com>
2023-01-18 14:40:45 +0200

    Migrate `setup.js` to TS

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.(js|ts)'],
-  setupFilesAfterEnv: ['./tests/setup.js'],
+  setupFilesAfterEnv: ['./tests/setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['example'],
   transform: {
