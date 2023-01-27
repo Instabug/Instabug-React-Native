10b59b86eb591a1bda7e1066ff1883e39d383eaa
Youssef Raafat <yraafat@instabug.com>
2023-01-27 16:11:26 +0200

    Remove Unnecessary Comment

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,10 +1,3 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 const path = require('path');
 const escape = require('escape-string-regexp');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
