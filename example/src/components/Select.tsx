bda967e60c628630ba85639f80c593e1fdbc6e16
Youssef Raafat <yraafat@instabug.com>
2023-02-01 16:30:30 +0200

    Add Select Component

---
+++ example/src/components/Select.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import { CheckIcon, Select as NativeBaseSelect } from 'native-base';
+
+interface SelectItemProps<T> {
+  label: string;
+  value: T;
+  isInitial?: boolean;
+}
+
+interface SelectProps<T> {
+  label: string;
+  items: SelectItemProps<T>[];
+  onValueChange: (value: T) => void;
+}
+
+export function Select<T>({ label, items, onValueChange }: SelectProps<T>) {
+  const initialItem = items.find((i) => i.isInitial) ?? items[0];
+  const [selectedItem, setSelectedItem] = useState(initialItem);
+
+  return (
+    <NativeBaseSelect
+      placeholder={label}
+      accessibilityLabel={label}
+      selectedValue={selectedItem.label}
+      onValueChange={(value) => {
+        const item = items.find((i) => i.label === value)!;
+        setSelectedItem(item);
+        onValueChange(item.value);
+      }}
+      _selectedItem={{
+        bg: 'coolGray.200',
+        endIcon: <CheckIcon size="4" />,
+      }}>
+      {items.map((item) => (
+        <NativeBaseSelect.Item key={item.label} label={item.label} value={item.label} />
+      ))}
+    </NativeBaseSelect>
+  );
+}
--- example/src/screens/SettingsScreen.tsx
+++ example/src/screens/SettingsScreen.tsx
@@ -1,43 +1,47 @@
 import React, { useState } from 'react';
 
 import Instabug, { BugReporting } from 'instabug-reactnative';
-import { CheckIcon, Input, InputGroup, InputLeftAddon, Select } from 'native-base';
+import { Input, InputGroup, InputLeftAddon } from 'native-base';
 
 import { ListTile } from '../components/ListTile';
 import { Screen } from '../components/Screen';
-
-const invocationEvents = [
-  { label: 'None', value: Instabug.invocationEvent.none },
-  { label: 'Shake', value: Instabug.invocationEvent.shake },
-  { label: 'Screenshot', value: Instabug.invocationEvent.screenshot },
-  { label: 'Two fingers swipe left', value: Instabug.invocationEvent.twoFingersSwipe },
-  { label: 'Floating button', value: Instabug.invocationEvent.floatingButton },
-];
+import { Select } from '../components/Select';
 
 export const SettingsScreen: React.FC = () => {
-  const [event, setEvent] = useState('Floating Button');
   const [color, setColor] = useState('1D82DC');
-  const [theme, setTheme] = useState('light');
 
   return (
     <Screen>
       <ListTile title="Invocation Event">
         <Select
-          accessibilityLabel="Invocation Event"
-          selectedValue={event}
+          label="Invocation Event"
+          items={[
+            {
+              label: 'None',
+              value: Instabug.invocationEvent.none,
+            },
+            {
+              label: 'Shake',
+              value: Instabug.invocationEvent.shake,
+            },
+            {
+              label: 'Screenshot',
+              value: Instabug.invocationEvent.screenshot,
+            },
+            {
+              label: 'Two fingers swipe left',
+              value: Instabug.invocationEvent.twoFingersSwipe,
+            },
+            {
+              label: 'Floating button',
+              value: Instabug.invocationEvent.floatingButton,
+              isInitial: true,
+            },
+          ]}
           onValueChange={(value) => {
-            setEvent(value);
-            const invocationEvent = invocationEvents.find((x) => x.label === value)!;
-            BugReporting.setInvocationEvents([invocationEvent?.value]);
+            BugReporting.setInvocationEvents([value]);
           }}
-          _selectedItem={{
-            bg: 'coolGray.200',
-            endIcon: <CheckIcon size="4" />,
-          }}>
-          {invocationEvents.map((x) => (
-            <Select.Item key={x.label} label={x.label} value={x.label} />
-          ))}
-        </Select>
+        />
       </ListTile>
 
       <ListTile title="Primary Color">
@@ -59,21 +63,19 @@ export const SettingsScreen: React.FC = () => {
 
       <ListTile title="Theme">
         <Select
-          accessibilityLabel="Theme"
-          selectedValue={theme}
-          onValueChange={(value) => {
-            setTheme(value);
-            Instabug.setColorTheme(
-              value === 'light' ? Instabug.colorTheme.light : Instabug.colorTheme.dark,
-            );
-          }}
-          _selectedItem={{
-            bg: 'coolGray.200',
-            endIcon: <CheckIcon size="4" />,
-          }}>
-          <Select.Item label="Light" value="light" />
-          <Select.Item label="Dark" value="dark" />
-        </Select>
+          label="Theme"
+          items={[
+            {
+              label: 'Light',
+              value: Instabug.colorTheme.light,
+            },
+            {
+              label: 'Dark',
+              value: Instabug.colorTheme.dark,
+            },
+          ]}
+          onValueChange={Instabug.setColorTheme}
+        />
       </ListTile>
     </Screen>
   );
