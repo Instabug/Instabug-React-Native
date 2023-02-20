f7d5d153be4b089a5f13d678c312bb3cc2a17487
Youssef Raafat <yraafat@instabug.com>
2023-02-20 14:03:06 +0200

    [MOB-11946] Delegate Native SDKs (#919)
    Allow native apps that have recently integrated React Native
    within their apps to use Instabug native SDKs via the
    `instabug-reactnative` npm package.

---
+++ ios/native.rb
@@ -0,0 +1,13 @@
+$instabug = { :version => '11.7.0' }
+
+def use_instabug! (spec = nil)
+  version = $instabug[:version]
+
+  if (!spec)
+    pod 'Instabug', version
+  else
+    spec.dependency 'Instabug', version
+  end
+
+  $instabug
+end
