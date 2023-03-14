3b82f31b3b2cf60370e66f8507d6206918cd3006
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-03-14 20:10:14 +0200

    Change Callbacks to Promises in APM

--- ios/RNInstabug/InstabugAPMBridge.m
+++ ios/RNInstabug/InstabugAPMBridge.m
@@ -66,13 +66,15 @@ RCT_EXPORT_METHOD(setAutoUITraceEnabled:(BOOL)isEnabled) {
     IBGAPM.autoUITraceEnabled = isEnabled;
 }
 
-RCT_EXPORT_METHOD(startExecutionTrace:(NSString *)name :(NSString *)id:(RCTResponseSenderBlock)callBack) {
+RCT_EXPORT_METHOD(startExecutionTrace:(NSString *)name :(NSString *)id
+                                     :(RCTPromiseResolveBlock)resolve
+                                     :(RCTPromiseRejectBlock)reject) {
     IBGExecutionTrace *trace = [IBGAPM startExecutionTraceWithName:name];
     if (trace != nil) {
         [traces setObject: trace forKey: id];
-        callBack(@[id]);
+        resolve(id);
     } else {
-        callBack(@[[NSNull null]]);
+        resolve([NSNull null]);
     }
 }
 
