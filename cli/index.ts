23dcd4b6f4fa15e72318c4be8da4a33b3c226e0b
Youssef Raafat <yraafat@instabug.com>
2023-03-21 16:10:49 +0200

    Fix CLI Typos

--- cli/index.ts
+++ cli/index.ts
@@ -6,7 +6,7 @@ import { uploadSourcemapsCommand } from './UploadSourcemaps';
 const program = new Command();
 
 program
-  .name('instaubg')
+  .name('instabug')
   .version('1.0.0-beta1')
   .description('A CLI for uploading source maps to Instabug dashboard.')
   .usage('[command]')
