f87cbc17a120ffd5d22310a73bea533168af448d
DavidMina96 <david.mina96@hotmail.com>
2023-03-23 12:00:43 +0200

    Fix formatting issues

--- examples/CodePush/__tests__/App-test.tsx
+++ examples/CodePush/__tests__/App-test.tsx
@@ -1,7 +1,6 @@
-import 'react-native';
-import React from 'react';
 import App from '../src/App';
-
+import React from 'react';
+import 'react-native';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
--- examples/CodePush/index.js
+++ examples/CodePush/index.js
@@ -1,5 +1,5 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
 import {name as appName} from './app.json';
+import App from './src/App';
+import {AppRegistry} from 'react-native';
 
 AppRegistry.registerComponent(appName, () => App);
