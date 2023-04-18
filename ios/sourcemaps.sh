314c77029b7f0c8f3169f1d5f6cb0f873a031040
DavidMina96 <david.mina96@hotmail.com>
2023-04-18 13:44:29 +0200

    Replace exit 1 with exit 0

--- ios/sourcemaps.sh
+++ ios/sourcemaps.sh
@@ -13,7 +13,7 @@ main() {
 
   if [[ -z "$INFOPLIST_FILE" ]] || [[ -z "$PROJECT_DIR" ]]; then
     echo "[Error] Instabug sourcemaps script must be invoked by Xcode"
-    exit 1
+    exit 0
   fi
 
   local source_map_file=$(generate_sourcemaps | tail -n 1)
@@ -51,7 +51,7 @@ generate_sourcemaps() {
 
   if [[ ! -f "$SOURCEMAP_FILE" ]]; then
     echo "[Error] Unable to find source map file at: $SOURCEMAP_FILE"
-    exit 1
+    exit 0
   fi
 
   echo $SOURCEMAP_FILE
@@ -74,7 +74,7 @@ resolve_var() {
 
   if [[ -z "$value" ]]; then
     echo "[Error] Unable to find $name! Set the environment variable \`$env_key\` and try again."
-    exit 1
+    exit 0
   fi
 
   echo $value
