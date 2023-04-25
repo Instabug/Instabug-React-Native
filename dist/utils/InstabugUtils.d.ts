c66e0adca59a6fcf52b69a4fe2fb59512b19c6c3
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-04-25 13:46:15 +0200

    Include build directories

---
+++ dist/utils/InstabugUtils.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="react-native" />
+import { ExtendedError, StackFrame } from 'react-native/Libraries/Core/Devtools/parseErrorStack';
+import type { NavigationState as NavigationStateV5, PartialState } from '@react-navigation/native';
+import type { NavigationState as NavigationStateV4 } from 'react-navigation';
+export declare const parseErrorStack: (error: ExtendedError) => StackFrame[];
+export declare const getActiveRouteName: (navigationState: NavigationStateV4) => string | null;
+declare function getFullRoute(state: NavigationStateV5 | PartialState<NavigationStateV5>): string;
+export declare const getStackTrace: (e: ExtendedError) => StackFrame[];
+export declare const captureJsErrors: () => void;
+export declare const stringifyIfNotString: (input: unknown) => string;
+export declare const invokeDeprecatedCallback: <T>(callback?: ((arg: T) => void) | undefined, arg?: T | null | undefined) => void;
+declare const _default: {
+    parseErrorStack: (error: ExtendedError) => StackFrame[];
+    captureJsErrors: () => void;
+    getActiveRouteName: (navigationState: NavigationStateV4) => string | null;
+    getFullRoute: typeof getFullRoute;
+    getStackTrace: (e: ExtendedError) => StackFrame[];
+    stringifyIfNotString: (input: unknown) => string;
+    invokeDeprecatedCallback: <T>(callback?: ((arg: T) => void) | undefined, arg?: T | null | undefined) => void;
+};
+export default _default;
