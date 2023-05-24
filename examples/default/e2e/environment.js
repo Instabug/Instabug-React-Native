9bf92f8f57740e8743ca41652a09d7283f16ffaf
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-05-24 11:21:42 +0300

    Upgrade example app to latest

--- examples/default/e2e/environment.js
+++ examples/default/e2e/environment.js
@@ -1,8 +1,4 @@
-const {
-  DetoxCircusEnvironment,
-  SpecReporter,
-  WorkerAssignReporter,
-} = require('detox/runners/jest-circus');
+const { DetoxCircusEnvironment } = require('detox/runners/jest');
 
 class CustomDetoxEnvironment extends DetoxCircusEnvironment {
   constructor(config, context) {
@@ -10,13 +6,6 @@ class CustomDetoxEnvironment extends DetoxCircusEnvironment {
 
     // Can be safely removed, if you are content with the default value (=300000ms)
     this.initTimeout = 300000;
-
-    // This takes care of generating status logs on a per-spec basis. By default, Jest only reports at file-level.
-    // This is strictly optional.
-    this.registerListeners({
-      SpecReporter,
-      WorkerAssignReporter,
-    });
   }
 }
 
