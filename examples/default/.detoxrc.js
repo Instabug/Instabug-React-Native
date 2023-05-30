d928669a29741e416b6e4249846d468c68d7baca
Ahmed Mahmoud <68241710+a7medev@users.noreply.github.com>
2023-05-30 13:30:32 +0300

    [MOB-12422] Upgrade Example App to React Native 0.71 (#982)
    Upgrade the default example app's React Native version to latest (0.71).

---
+++ examples/default/.detoxrc.js
@@ -0,0 +1,68 @@
+/** @type {Detox.DetoxConfig} */
+module.exports = {
+  testRunner: {
+    $0: 'jest',
+    args: {
+      config: 'e2e/config.json',
+      _: ['e2e'],
+    },
+  },
+  apps: {
+    'ios.debug': {
+      type: 'ios.app',
+      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/InstabugExample.app',
+      build:
+        'xcodebuild -workspace ios/InstabugExample.xcworkspace -scheme InstabugExample -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build',
+    },
+    'ios.release': {
+      type: 'ios.app',
+      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/InstabugExample.app',
+      build:
+        'xcodebuild -workspace ios/InstabugExample.xcworkspace -scheme InstabugExample -configuration Release -sdk iphonesimulator -derivedDataPath ios/build',
+    },
+    'android.debug': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      build:
+        'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..',
+    },
+    'android.release': {
+      type: 'android.apk',
+      binaryPath: 'android/app/build/outputs/apk/release/app-release.apk',
+      build:
+        'cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release && cd ..',
+    },
+  },
+  devices: {
+    simulator: {
+      type: 'ios.simulator',
+      device: {
+        type: 'iPhone 13 Pro Max',
+      },
+    },
+    emulator: {
+      type: 'android.emulator',
+      device: {
+        avdName: 'Nexus_6P_API_27',
+      },
+    },
+  },
+  configurations: {
+    'ios.sim.debug': {
+      device: 'simulator',
+      app: 'ios.debug',
+    },
+    'ios.sim.release': {
+      device: 'simulator',
+      app: 'ios.release',
+    },
+    'android.emu.debug': {
+      device: 'emulator',
+      app: 'android.debug',
+    },
+    'android.emu.release': {
+      device: 'emulator',
+      app: 'android.release',
+    },
+  },
+};
