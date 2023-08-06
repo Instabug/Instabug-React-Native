e0a0931f3eb761b798c9c493b20639154e3be899
AbdelHamid Nasser <anasser@instabug.com>
2023-08-07 01:21:59 +0300

    🔧 chore(detox): Add auto simulator detection
    📎: [MOB-12654](https://instabug.atlassian.net/browse/MOB-12654)
    
    ℹ️: Add detection for available simulators and pick the last one available to run the E2E tests.

--- examples/default/.detoxrc.js
+++ examples/default/.detoxrc.js
@@ -1,4 +1,35 @@
 /** @type {Detox.DetoxConfig} */
+
+const { execSync } = require('child_process');
+
+// Function to get the iOS simulator device type
+function getSimulatorDeviceType() {
+  try {
+
+     /// Execute 'xcrun simctl list devices' command to get the list of iOS simulator devices
+     const allSimulatorDevices = execSync('xcrun simctl list devices').toString();
+
+     /// Filter the output to get the list of iPhone devices
+     const filteredDevices = allSimulatorDevices.match(/iPhone[^)]*/g);
+
+     /// Clean the device names to get the final device type
+     const cleanedDevices = filteredDevices.map(device => device.replace(/\(.*$/, '').trim());
+
+     /// Assuming you want to use the last device from the list, you can modify this logic as needed
+     const lastDeviceType = cleanedDevices[cleanedDevices.length - 1];
+
+     /// Remove the trailing newline character from the device type
+     const trimmedDeviceType = lastDeviceType.replace('\n', '');
+
+     /// Return the final device type
+     return trimmedDeviceType;
+  } catch (error) {
+    console.error('Error while fetching iOS simulator device list:', error);
+    return null;
+  }
+}
+
+
 module.exports = {
   testRunner: {
     $0: 'jest',
@@ -37,13 +68,13 @@ module.exports = {
     simulator: {
       type: 'ios.simulator',
       device: {
-        type: 'iPhone 13 Pro Max',
+        type: getSimulatorDeviceType(),
       },
     },
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Nexus_6P_API_27',
+        avdName: getAVDName(),
       },
     },
   },
