9bf30f064cf7ff195ffd7480488a261ec737b313
Abd ElHamid Nasser <abdelhamidnasser@Abds-MacBook-Air.local>
2023-08-17 01:09:07 +0300

    refactor: extract shared crash reporting logic (#1010)
    Jira ID: MOB-12538
    
    - refactor: extract duplicate crash reporting code
    - chore: Resolve utils lint issues
    - test: add sendCrashReport tests in utils
    - test: refactor tests validating sendCrashReport
    - chore: exclude local history from .gitignore
    - test: refactor errorMock to use TypeError

--- test/modules/CrashReporting.spec.ts
+++ test/modules/CrashReporting.spec.ts
@@ -1,9 +1,8 @@
 import '../mocks/mockInstabugUtils';
 
-import { Platform } from 'react-native';
-
 import * as CrashReporting from '../../src/modules/CrashReporting';
 import { NativeCrashReporting } from '../../src/native/NativeCrashReporting';
+import InstabugUtils from '../../src/utils/InstabugUtils';
 
 describe('CrashReporting Module', () => {
   it('should call the native method setEnabled', () => {
@@ -13,39 +12,14 @@ describe('CrashReporting Module', () => {
     expect(NativeCrashReporting.setEnabled).toBeCalledWith(true);
   });
 
-  it('should call the native method sendHandledJSCrash with JSON object when platform is iOS', () => {
-    Platform.OS = 'ios';
-    const error = { name: 'TypeError', message: 'Invalid type' };
+  it('should call the native method sendCrashReporting with JSON object and sendHandledJsCrash as a callback', () => {
+    const error = new TypeError('Invalid type');
     CrashReporting.reportError(error);
 
-    const expected = {
-      message: 'TypeError - Invalid type',
-      e_message: 'Invalid type',
-      e_name: 'TypeError',
-      os: 'ios',
-      platform: 'react_native',
-      exception: 'javascriptStackTrace',
-    };
-
-    expect(NativeCrashReporting.sendHandledJSCrash).toBeCalledTimes(1);
-    expect(NativeCrashReporting.sendHandledJSCrash).toBeCalledWith(expected);
-  });
-
-  it('should call the native method sendHandledJSCrash with stringified JSON object when platform is Android', () => {
-    Platform.OS = 'android';
-    const error = { name: 'TypeError', message: 'Invalid type' };
-    CrashReporting.reportError(error);
-
-    const expected = JSON.stringify({
-      message: 'TypeError - Invalid type',
-      e_message: 'Invalid type',
-      e_name: 'TypeError',
-      os: 'android',
-      platform: 'react_native',
-      exception: 'javascriptStackTrace',
-    });
-
-    expect(NativeCrashReporting.sendHandledJSCrash).toBeCalledTimes(1);
-    expect(NativeCrashReporting.sendHandledJSCrash).toBeCalledWith(expected);
+    expect(InstabugUtils.sendCrashReport).toBeCalledTimes(1);
+    expect(InstabugUtils.sendCrashReport).toBeCalledWith(
+      error,
+      NativeCrashReporting.sendHandledJSCrash,
+    );
   });
 });
