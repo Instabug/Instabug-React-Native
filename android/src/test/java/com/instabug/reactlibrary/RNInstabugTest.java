8c928c2736748abf70840e5f33bdf5e656af5d47
AbdElHamid Nasser <anasser@instabug.com>
2023-08-23 17:50:49 +0300

    test(android): add tests for native sdk init APIs
    Add tests fpr `RNInstabug` native `init` API, `setCurrentPlatform` and `setDeprecationBaseUrl`.
    
    Jira ID: MOB-12628, MOB-12689, MOB-12690

---
+++ android/src/test/java/com/instabug/reactlibrary/RNInstabugTest.java
@@ -0,0 +1,147 @@
+package com.instabug.reactlibrary;
+
+
+import static com.instabug.reactlibrary.util.GlobalMocks.reflected;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.app.Application;
+
+import com.instabug.library.Instabug;
+import com.instabug.library.LogLevel;
+import com.instabug.library.Platform;
+import com.instabug.library.invocation.InstabugInvocationEvent;
+import com.instabug.reactlibrary.util.GlobalMocks;
+import com.instabug.reactlibrary.util.MockReflected;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+
+
+// Todo: Rename it to match the class name
+public class RNInstabugTest {
+    private final Application mContext = mock(Application.class);
+
+    private MockedStatic<Instabug> mInstabug;
+
+    private RNInstabug sut;
+
+
+    @Before
+    public void setUp() throws Exception {
+        sut = spy(RNInstabug.getInstance());
+        mInstabug = mockStatic(Instabug.class);
+
+        GlobalMocks.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mInstabug.close();
+        GlobalMocks.close();
+    }
+
+    @Test
+    public void testInit() {
+        final InstabugInvocationEvent[] invocationEvents = new InstabugInvocationEvent[]{InstabugInvocationEvent.FLOATING_BUTTON};
+        final String token = "fde....";
+        final int logLevel = LogLevel.VERBOSE;
+
+        MockedConstruction<Instabug.Builder> mInstabugBuilder = mockConstruction(
+                Instabug.Builder.class, (mock, context) -> {
+                    String actualToken = (String) context.arguments().get(1);
+                    // Initializes Instabug with the correct token
+                    assertEquals(token, actualToken);
+                    when(mock.setSdkDebugLogsLevel(anyInt())).thenReturn(mock);
+                    when(mock.setInvocationEvents(any())).thenReturn(mock);
+                });
+
+        sut.init(mContext, token, logLevel);
+
+        Instabug.Builder builder = mInstabugBuilder.constructed().get(0);
+
+        // Originally it is created as `LogLevelError` then after calling `setSdkDebugLogsLevel` it is changed to the value `logLevel` property
+        assertEquals(
+                "expected Instabug to be initialized using Instabug.Builder",
+                LogLevel.ERROR,
+                mInstabugBuilder.constructed().size()
+        );
+
+        // Here we check that it has changed to verbose value of the `logLevel` property
+        verify(builder).setSdkDebugLogsLevel(LogLevel.VERBOSE);
+        verify(builder).build();
+
+
+
+        verify(sut).setBaseUrlForDeprecationLogs();
+        verify(sut).setCurrentPlatform();
+    }
+
+    @Test
+    public void testSetCurrentPlatform() {
+        sut.setCurrentPlatform();
+
+        reflected.verify(() -> MockReflected.setCurrentPlatform(Platform.RN));
+    }
+
+    @Test
+    public void testSetDeprecationBaseUrl() {
+        sut.setBaseUrlForDeprecationLogs();
+
+        reflected.verify(() -> MockReflected.setBaseUrl(any()));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
