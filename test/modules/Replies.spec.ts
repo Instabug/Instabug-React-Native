1f263272bc14181688a392ca7960b28fd12ee6ee
AbdElHamid Nasser <anasser@instabug.com>
2023-08-24 12:25:03 +0300

    chore: bump up react-native dependencies (#1007)
    Jira ID: MOB-12699
    
    - ⚙️ build: upgrade ci Node.js to v18.17.0
    - 🪮 style: fix format issues
    - 🔧 refactor: migrate deprecated `mocked` imports in favor of `jest-mock`
    - ⬆️ chore: bump SDK dependencies (all but TypeScript)
    - ✈️ chore(android): migrate to RN 0.72.3
    - ⬆️ chore(example): bump react-native, jest and typescript

--- test/modules/Replies.spec.ts
+++ test/modules/Replies.spec.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
-import { mocked } from 'ts-jest/utils';
+import { mocked } from 'jest-mock';
 
 import * as Replies from '../../src/modules/Replies';
 import { NativeEvents, NativeReplies, emitter } from '../../src/native/NativeReplies';
