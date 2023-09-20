c54b1540b65ab16fdbc317a547a878264237e7b1
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-20 11:51:10 +0300

    fix: multiline variable strings in shell scripts

--- scripts/snapshot-branch.sh
+++ scripts/snapshot-branch.sh
@@ -3,20 +3,20 @@
 pr_url="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/pulls?head=$CIRCLE_PROJECT_USERNAME:$CIRCLE_BRANCH&state=open"
 pr_response=$(curl --location --request GET "$pr_url" --header "Authorization: Bearer $RELEASE_GITHUB_TOKEN")
 
-if [ $(echo $pr_response | jq length) -eq 0 ]; then
+if [ $(echo "$pr_response" | jq length) -eq 0 ]; then
   echo "No PR found, proceeding with branch name instead"
 
   SNAPSHOT_BRANCH="snapshot/$CIRCLE_BRANCH"
 else
-  pr_description=$(echo $pr_response | jq -r ".[].body")
+  pr_description=$(echo "$pr_response" | jq -r '.[].body')
 
-  SNAPSHOT_BRANCH=$(echo -E $pr_description | grep 'Snapshot name:' | cut -d ':' -f 2 | xargs)
+  SNAPSHOT_BRANCH=$(echo -E "$pr_description" | grep 'Snapshot name:' | cut -d ':' -f 2 | xargs)
 
   if [ -z "$SNAPSHOT_BRANCH" ]; then
     echo "No custom snapshot name found, proceeding with default snapshot naming convention"
 
     version=$(jq -r '.version' package.json)
-    jira_id=$(echo -E $pr_description | grep 'Jira ID:' | grep -Eo '[A-Z]+-[0-9]+')
+    jira_id=$(echo -E "$pr_description" | grep 'Jira ID:' | grep -Eo '[A-Z]+-[0-9]+')
 
     if [ -z "$jira_id" ]; then
       echo "No Jira ID found, proceeding with branch name instead"
