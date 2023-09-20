5ff5c286112ddff622467b1ab223caa14e2c2c22
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-09-20 11:21:57 +0300

    ci: allow customizing snapshot branch

--- scripts/snapshot-branch.sh
+++ scripts/snapshot-branch.sh
@@ -10,14 +10,20 @@ if [ $(echo $pr_response | jq length) -eq 0 ]; then
 else
   pr_description=$(echo $pr_response | jq -r ".[].body")
 
-  version=$(jq -r '.version' package.json)
-  jira_id=$(echo -E $pr_description | grep 'Jira ID:' | grep -Eo '[A-Z]+-[0-9]+')
+  SNAPSHOT_BRANCH=$(echo -E $pr_description | grep 'Snapshot name:' | cut -d ':' -f 2 | xargs)
 
-  if [ -z "$jira_id" ]; then
-    echo "No Jira ID found, proceeding with branch name instead"
+  if [ -z "$SNAPSHOT_BRANCH" ]; then
+    echo "No custom snapshot name found, proceeding with default snapshot naming convention"
 
-    SNAPSHOT_BRANCH="snapshot/$CIRCLE_BRANCH"
-  else
-    SNAPSHOT_BRANCH="snapshot/$version-$jira_id"
+    version=$(jq -r '.version' package.json)
+    jira_id=$(echo -E $pr_description | grep 'Jira ID:' | grep -Eo '[A-Z]+-[0-9]+')
+
+    if [ -z "$jira_id" ]; then
+      echo "No Jira ID found, proceeding with branch name instead"
+
+      SNAPSHOT_BRANCH="snapshot/$CIRCLE_BRANCH"
+    else
+      SNAPSHOT_BRANCH="snapshot/$version-$jira_id"
+    fi
   fi
 fi
