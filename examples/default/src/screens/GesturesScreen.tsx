4918da7dcc192dfe887e8a9aa326acb1b684aedf
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2023-10-31 22:21:44 +0200

    feat(example): add swipe example

--- examples/default/src/screens/GesturesScreen.tsx
+++ examples/default/src/screens/GesturesScreen.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import { Swipeable } from 'react-native-gesture-handler';
+import { Box, Center, Text } from 'native-base';
+
 import { Screen } from '../components/Screen';
+import { Section } from '../components/Section';
 
 export const GesturesScreen: React.FC = () => {
-  return <Screen />;
+  return (
+    <Screen>
+      <Section title="Swipe">
+        <Swipeable
+          renderLeftActions={() => <Box bg="red.600" height={100} width={100} />}
+          renderRightActions={() => <Box bg="green.600" height={100} width={100} />}>
+          <Center bg="primary.600" height={100}>
+            <Text color="white" fontSize="md">
+              Swipe Me
+            </Text>
+          </Center>
+        </Swipeable>
+      </Section>
+    </Screen>
+  );
 };
