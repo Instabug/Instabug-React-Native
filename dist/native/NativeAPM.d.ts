db6abc51226a878d929be2586d89f64f09567a6f
Instabug-CP-CI <github-ci+cp@instabug.com>
2023-11-01 17:09:27 +0000

    chore: add generate files

---
+++ dist/native/NativeAPM.d.ts
@@ -0,0 +1,15 @@
+import type { NativeModule } from 'react-native';
+export interface ApmNativeModule extends NativeModule {
+    setEnabled(isEnabled: boolean): void;
+    networkLog(data: string): void;
+    setAppLaunchEnabled(isEnabled: boolean): void;
+    endAppLaunch(): void;
+    startExecutionTrace(name: string, timestamp: string): Promise<string | null>;
+    setExecutionTraceAttribute(id: string, key: string, value: string): void;
+    endExecutionTrace(id: string): void;
+    setAutoUITraceEnabled(isEnabled: boolean): void;
+    startUITrace(name: string): void;
+    endUITrace(): void;
+    ibgSleep(): void;
+}
+export declare const NativeAPM: ApmNativeModule;
