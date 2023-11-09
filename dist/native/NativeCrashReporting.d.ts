976c1830f42e6a7d8131b9d63d9f68c2df0d8c81
Instabug-CP-CI <github-ci+cp@instabug.com>
2023-11-09 13:31:06 +0000

    chore: add generate files

---
+++ dist/native/NativeCrashReporting.d.ts
@@ -0,0 +1,16 @@
+import type { NativeModule, Platform } from 'react-native';
+import type { StackFrame } from 'react-native/Libraries/Core/Devtools/parseErrorStack';
+export interface CrashData {
+    message: string;
+    e_message: string;
+    e_name: string;
+    os: (typeof Platform)['OS'];
+    platform: 'react_native';
+    exception: StackFrame[];
+}
+export interface CrashReportingNativeModule extends NativeModule {
+    setEnabled(isEnabled: boolean): void;
+    sendJSCrash(data: CrashData | string): Promise<void>;
+    sendHandledJSCrash(data: CrashData | string): Promise<void>;
+}
+export declare const NativeCrashReporting: CrashReportingNativeModule;
