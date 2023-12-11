1fe20cc6c9758ef80cd138c92ec6822da9b63bc7
AbdElHamid Nasser <anasser@instabug.com>
2023-12-11 16:29:18 +0200

    feat(ios): support code push

--- ios/RNInstabug/RNInstabug.h
+++ ios/RNInstabug/RNInstabug.h
@@ -8,6 +8,17 @@
 + (void)initWithToken:(NSString *)token invocationEvents:(IBGInvocationEvent)invocationEvents debugLogsLevel:(IBGSDKDebugLogsLevel)debugLogsLevel;
 + (void)initWithToken:(NSString *)token invocationEvents:(IBGInvocationEvent)invocationEvents;
 
+/**
+ @brief Set codePush version before starting the SDK.
+ 
+ @discussion Sets Code Push version to be used for all reports.
+ should be called from `-[UIApplicationDelegate application:didFinishLaunchingWithOptions:]`
+ and before `startWithToken`.
+ 
+ @param codePushVersion the Code Push version to be used for all reports.
+ */
++ (void)setCodePushVersion:(NSString *)codePushVersion;
+
 @end
 
 #endif /* RNInstabug_h */
