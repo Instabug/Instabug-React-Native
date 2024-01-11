dcf60ff32182e6974c9419fe5b805579c8144359
Ahmed alaa <aalaa@instabug.com>
2024-01-11 19:18:25 +0200

    chore_remove_all_locales_except_english_locale

--- scripts/dream-11-delete-unused-features.sh
+++ scripts/dream-11-delete-unused-features.sh
@@ -4,21 +4,21 @@
 deletedFeaturesFilesInJavaScript=("Surveys" "FeatureRequests" "Survey")
 for feature in "${deletedFeaturesFilesInJavaScript[@]}";
  do
-rm -f  ../src/modules/"$feature".ts
-rm -f ../test/mocks/mock"$feature".ts
-sed -i "s/import..*$feature';//g"  ../src/index.ts
-sed -i "s/$feature,//g"  ../src/index.ts
+rm -f  src/modules/"$feature".ts
+rm -f test/mocks/mock"$feature".ts
+sed -i "s/import..*$feature';//g"  src/index.ts
+sed -i "s/$feature,//g"  src/index.ts
 
 done
-npx eslint  ../src/index.ts --fix
+npx eslint  src/index.ts --fix
 
 # remove survey and featureRequest features in Android  files
 deletedFeaturesFilesInAndroidApp=("RNInstabugSurveysModule" "RNInstabugFeatureRequestsModule")
 for feature in "${deletedFeaturesFilesInAndroidApp[@]}";
  do
-rm -f ../android/src/main/java/com/instabug/reactlibrary/"$feature".java
-rm -f ../android/src/test/java/com/instabug/reactlibrary/"$feature"Test.java
-sed -i "s/modules.add(new $feature(reactContext));//g"  ../android/src/main/java/com/instabug/reactlibrary/RNInstabugReactnativePackage.java
+rm -f android/src/main/java/com/instabug/reactlibrary/"$feature".java
+rm -f android/src/test/java/com/instabug/reactlibrary/"$feature"Test.java
+sed -i "s/modules.add(new $feature(reactContext));//g"  android/src/main/java/com/instabug/reactlibrary/RNInstabugReactnativePackage.java
 
 done
 
@@ -26,13 +26,13 @@ done
 deletedFeaturesFilesInIosApp=("InstabugSurveysBridge" "InstabugFeatureRequestsBridge")
 for feature in "${deletedFeaturesFilesInIosApp[@]}";
  do
-rm -f ../ios/RNInstabug/"$feature".h
-rm -f ../ios/RNInstabug/"$feature".m
+rm -f ios/RNInstabug/"$feature".h
+rm -f ios/RNInstabug/"$feature".m
 done
 
 
-#rm -rf '../examples'
+#rm -rf 'examples'
 
 # remove all locales except English locale
-sed -i -E '/english/!s/.*constants.locale.*//g' ../src/utils/Enums.ts
-npx eslint  ../src/index.ts --fix ../src/utils/Enums.ts
+sed -i -E '/english/!s/.*constants.locale.*//g' src/utils/Enums.ts
+npx eslint  src/index.ts --fix src/utils/Enums.ts
