12263619d12aae91929d985069e14e373e7ba681
ahmedAlaaInstabug <154802748+ahmedAlaaInstabug@users.noreply.github.com>
2024-01-17 20:54:53 +0200

    chore: add d11 release pipeline to ci (#1093)
    * chore: create d11 customization script (#1090)
    
    * chore_add_removing_native_code_in_d11_customization_script
    
    * chore_add_dream-11-delete-unused-features
    
    Jira ID: MOB-13551
    
    * chore_add_d11_release_pipeline_to_circle_ci
    
    * chore_add_d11_release_pipeline(test)
    
    
    * chore_add_d11_release_pipeline(test)
    
    
    * chore_remove_all_locales_except_english_locale
    
    * fix_change_d11_sdk_name
    
    
    * chore_remove_d11_test_workflow_in_ci
    
    * chore_add_d11_release_pipeline_to_circle_ci
    
    * chore_add_d11_release_pipeline(test)
    
    * chore_remove_all_locales_except_english_locale
    
    * fix_change_d11_sdk_name
    
    * chore_remove_d11_test_workflow_in_ci

---
+++ scripts/dream-11-delete-unused-features.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# remove survey and featureRequest features in JavaScript files
+deletedFeaturesFilesInJavaScript=("Surveys" "FeatureRequests" "Survey")
+for feature in "${deletedFeaturesFilesInJavaScript[@]}";
+ do
+   echo "$feature"
+rm -f  src/modules/"$feature".ts
+rm -f test/mocks/mock"$feature".ts
+sed -i "s/import..*$feature';//g"  src/index.ts
+sed -i "s/$feature,//g"  src/index.ts
+
+done
+npx eslint  src/index.ts --fix
+
+# remove survey and featureRequest features in Android  files
+deletedFeaturesFilesInAndroidApp=("RNInstabugSurveysModule" "RNInstabugFeatureRequestsModule")
+for feature in "${deletedFeaturesFilesInAndroidApp[@]}";
+ do
+      echo "$feature"
+
+rm -f android/src/main/java/com/instabug/reactlibrary/"$feature".java
+rm -f android/src/test/java/com/instabug/reactlibrary/"$feature"Test.java
+sed -i "s/modules.add(new $feature(reactContext));//g"  android/src/main/java/com/instabug/reactlibrary/RNInstabugReactnativePackage.java
+
+done
+
+# remove survey and featureRequest features in IOS  files
+deletedFeaturesFilesInIosApp=("InstabugSurveysBridge" "InstabugFeatureRequestsBridge")
+for feature in "${deletedFeaturesFilesInIosApp[@]}";
+ do
+      echo "$feature"
+rm -f ios/RNInstabug/"$feature".h
+rm -f ios/RNInstabug/"$feature".m
+done
+sed -i "s/\#import <Instabug\/IBGSurveys.h>//g"  ios/RNInstabug/InstabugReactBridge.m
+sed -i "s/\#import <Instabug\/IBGSurveys.h>//g"  ios/RNInstabug/InstabugReactBridge.h
+
+
+# remove all locales except English locale
+sed -i -E '/english/!s/.*constants.locale.*//g' src/utils/Enums.ts
+npx eslint  src/index.ts --fix src/utils/Enums.ts
