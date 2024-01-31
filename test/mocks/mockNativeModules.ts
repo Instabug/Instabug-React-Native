dd7bea500b0381d09f84b7815df3ac67c12d4a4c
Ahmed alaa <aalaa@instabug.com>
2024-01-31 04:19:40 +0200

    fix tests

--- test/mocks/mockNativeModules.ts
+++ test/mocks/mockNativeModules.ts
@@ -2,11 +2,9 @@ import type { InstabugNativePackage } from '../../src/native/NativePackage';
 import mockAPM from './mockAPM';
 import mockBugReporting from './mockBugReporting';
 import mockCrashReporting from './mockCrashReporting';
-import mockFeatureRequests from './mockFeatureRequests';
 import mockSessionReplay from './mockSessionReplay';
 import mockInstabug from './mockInstabug';
 import mockReplies from './mockReplies';
-import mockSurveys from './mockSurveys';
 
 jest.mock('react-native', () => {
   const RN = jest.requireActual('react-native');
@@ -14,11 +12,9 @@ jest.mock('react-native', () => {
     IBGAPM: mockAPM,
     IBGBugReporting: mockBugReporting,
     IBGCrashReporting: mockCrashReporting,
-    IBGFeatureRequests: mockFeatureRequests,
     IBGSessionReplay: mockSessionReplay,
     Instabug: mockInstabug,
     IBGReplies: mockReplies,
-    IBGSurveys: mockSurveys,
   };
 
   Object.assign(RN.NativeModules, mockNativeModules);
