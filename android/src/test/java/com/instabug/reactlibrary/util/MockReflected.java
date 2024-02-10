457c0d373d8064c3ff15d195b1d32ce8a57ad63a
AbdElHamid Nasser <anasser@instabug.com>
2024-02-10 10:18:06 +0200

    feat: add network diagnostics callback
    Jira ID: MOB-13680

--- android/src/test/java/com/instabug/reactlibrary/util/MockReflected.java
+++ android/src/test/java/com/instabug/reactlibrary/util/MockReflected.java
@@ -1,5 +1,13 @@
 package com.instabug.reactlibrary.util;
 
+import static com.instabug.reactlibrary.utils.InstabugUtil.getMethod;
+
+import android.graphics.Bitmap;
+
+import com.instabug.library.networkDiagnostics.model.NetworkDiagnosticsCallback;
+
+import java.lang.reflect.Method;
+
 /**
  * Includes fake implementations of methods called by reflection.
  * Used to verify whether or not a private methods was called.
@@ -16,4 +24,19 @@ public class MockReflected {
      * Instabug.util.InstabugDeprecationLogger.setBaseUrl
      */
     public static void setBaseUrl(String baseUrl) {}
+
+    /**
+     * com.instabug.library.Instabug.setNetworkDiagnosticsCallback
+     */
+    public static void setNetworkDiagnosticsCallback(NetworkDiagnosticsCallback callback) {}
+
+    /**
+     * com.instabug.library.Instabug.reportCurrentViewChange
+     */
+    public static void reportCurrentViewChange(String currentView) {}
+
+    /**
+     * com.instabug.library.Instabug.reportScreenChange
+     */
+    public static void reportScreenChange(Bitmap screenshot, String screen) {}
 }
