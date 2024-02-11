d3c769e743c5fe309d8c464eed62388df872dd49
Ahmed alaa <aalaa@instabug.com>
2024-02-11 20:17:05 +0200

    feat_add_setNDKCrashesEnabled

--- android/src/test/java/com/instabug/reactlibrary/RNInstabugCrashReportingModuleTest.java
+++ android/src/test/java/com/instabug/reactlibrary/RNInstabugCrashReportingModuleTest.java
@@ -1,9 +1,85 @@
 package com.instabug.reactlibrary;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+
+import android.os.Looper;
+
+import com.instabug.crash.CrashReporting;
+import com.instabug.library.Feature;
+import com.instabug.reactlibrary.util.GlobalMocks;
+import com.instabug.reactlibrary.utils.MainThreadHandler;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 
 public class RNInstabugCrashReportingModuleTest {
+    private final RNInstabugCrashReportingModule rnModule = new RNInstabugCrashReportingModule(null);
+
+    // Mock Objects
+    private MockedStatic<Looper> mockLooper;
+    private MockedStatic<MainThreadHandler> mockMainThreadHandler;
+    private MockedStatic<CrashReporting> mockCrashReporting;
+
+
+    @Before
+    public void mockMainThreadHandler() throws Exception {
+        // Mock static functions
+        mockLooper = mockStatic(Looper.class);
+        mockMainThreadHandler = mockStatic(MainThreadHandler.class);
+        mockCrashReporting = mockStatic(CrashReporting.class);
+        // Mock Looper class
+        Looper mockMainThreadLooper = mock(Looper.class);
+        Mockito.when(Looper.getMainLooper()).thenReturn(mockMainThreadLooper);
+
+
+        // Override runOnMainThread
+        Answer<Boolean> handlerPostAnswer = new Answer<Boolean>() {
+            @Override
+            public Boolean answer(InvocationOnMock invocation) throws Throwable {
+                invocation.getArgument(0, Runnable.class).run();
+                return true;
+            }
+        };
+        Mockito.doAnswer(handlerPostAnswer).when(MainThreadHandler.class);
+        MainThreadHandler.runOnMainThread(any(Runnable.class));
+    }
+
+    @After
+    public void tearDown() {
+        // Remove static mocks
+        mockLooper.close();
+        mockMainThreadHandler.close();
+        mockCrashReporting.close();
+    }
+
+    /********Crashes*********/
+
+    @Test
+    public void givenTrue$SetNDKCrashesEnabledShouldCallNativeApiWithEnable() {
+        // when
+        rnModule.setNDKCrashesEnabled(true);
+
+//then
+        mockCrashReporting.verify(() -> CrashReporting.setNDKCrashesState(Feature.State.ENABLED));
+    }
+
+    @Test
+    public void givenTrue$SetNDKCrashesEnabledShouldCallNativeApiWithDisabled() {
+        // when
+        rnModule.setNDKCrashesEnabled(false);
+
+        //then
+        mockCrashReporting.verify(() -> CrashReporting.setNDKCrashesState(Feature.State.DISABLED));
+    }
+
     @Test
     public void givenString$sendHandledJSCrash_whenQuery_thenShouldCallNativeApiWithArgs() throws Exception {
 //        JSONObject json = mock(JSONObject.class);
--- src/modules/CrashReporting.ts
+++ src/modules/CrashReporting.ts
@@ -2,6 +2,7 @@ import type { ExtendedError } from 'react-native/Libraries/Core/Devtools/parseEr
 
 import { NativeCrashReporting } from '../native/NativeCrashReporting';
 import InstabugUtils from '../utils/InstabugUtils';
+import { Platform } from 'react-native';
 
 /**
  * Enables and disables everything related to crash reporting including intercepting
@@ -17,5 +18,15 @@ export const setEnabled = (isEnabled: boolean) => {
  * @param error Error object to be sent to Instabug's servers
  */
 export const reportError = (error: ExtendedError) => {
-  return InstabugUtils.sendCrashReport(error, NativeCrashReporting.sendHandledJSCrash);
+  InstabugUtils.sendCrashReport(error, NativeCrashReporting.sendHandledJSCrash);
+};
+
+/**
+ * Enables and disables capturing native C++ NDK crashes.
+ * @param isEnabled
+ */
+export const setNDKCrashesEnabled = (isEnabled: boolean) => {
+  if (Platform.OS === 'android') {
+    NativeCrashReporting.setNDKCrashesEnabled(isEnabled);
+  }
 };
