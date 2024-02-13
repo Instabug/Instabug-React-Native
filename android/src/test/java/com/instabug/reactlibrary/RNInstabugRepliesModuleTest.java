e9b78242137787241b9fed4fa7bfce13428e24c4
Ahmed alaa <aalaa@instabug.com>
2024-02-13 02:35:51 +0200

    fix android_testing issue

--- android/src/test/java/com/instabug/reactlibrary/RNInstabugRepliesModuleTest.java
+++ android/src/test/java/com/instabug/reactlibrary/RNInstabugRepliesModuleTest.java
@@ -74,6 +74,7 @@ public class RNInstabugRepliesModuleTest {
         mockLooper.close();
         mockMainThreadHandler.close();
         mockReplies.close();
+        mockInstabugUtil.close();
     }
 
     /********Replies*********/
