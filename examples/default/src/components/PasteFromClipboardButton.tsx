b38c372a91183aa5fb6d512e6448c2bbf24f33c2
AbdElHamid Nasser <anasser@instabug.com>
2024-02-20 17:42:11 +0200

    feat(example): add apm screen
    Support network traces, app flows, execution traces.

---
+++ examples/default/src/components/PasteFromClipboardButton.tsx
@@ -0,0 +1,24 @@
+import Clipboard from '@react-native-community/clipboard';
+import { StyleProp, TextStyle, TouchableHighlight } from 'react-native';
+import React, { PropsWithChildren } from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface PasteFromClipboardButtonProps extends PropsWithChildren {
+  onPress?: (text: string) => void;
+  style?: StyleProp<TextStyle>;
+}
+
+const PasteFromClipboardButton: React.FC<PasteFromClipboardButtonProps> = ({ onPress, style }) => {
+  const handlePress = async () => {
+    const text = await Clipboard.getString();
+    onPress?.call(undefined, text); // Using call to pass the text to the callback
+  };
+
+  return (
+    <TouchableHighlight onPress={handlePress} style={style}>
+      <Icon name="copy-outline" />
+    </TouchableHighlight>
+  );
+};
+
+export default PasteFromClipboardButton;
