802132da58ffaeb9fb843d876c41edcbafeef7b7
ahmed alaa <154802748+ahmedAlaaInstabug@users.noreply.github.com>
2024-02-22 17:58:52 +0200

    feat(example): add google maps screen (#1146)

--- examples/default/ios/InstabugExample/AppDelegate.mm
+++ examples/default/ios/InstabugExample/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -10,6 +11,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+ [GMSServices provideAPIKey:@"API KEY"]; // use the api key obtained from Google Console
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
