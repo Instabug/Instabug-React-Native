1b2e01d9628b42167ba5d9ccd29658b01691c64d
ahmed alaa <154802748+ahmedAlaaInstabug@users.noreply.github.com>
2024-02-25 03:08:04 +0200

    feat: add new NDK crash scenarios to example app (#1139)
    * add: crashes examples to simple app
    
    * feat(example): enable NDK crash

---
+++ examples/default/android/app/src/main/cpp/crasher_2.h
@@ -0,0 +1,58 @@
+
+
+/************* SIGSEGV *******************************/
+void causeSIGSEGVCrashF3();
+
+void causeSIGSEGVCrashF2();
+
+void causeSIGSEGVCrashF1();
+
+/*****************************************************/
+
+/************* SIGABRT *******************************/
+void causeSIGABRTCrashF3();
+
+void causeSIGABRTCrashF2();
+
+void causeSIGABRTCrashF1();
+
+/****************************************************/
+
+/************* SIGFPE *******************************/
+
+int causeSIGFPECrashF3();
+
+void causeSIGFPECrashF2();
+
+void causeSIGFPECrashF1();
+
+/***************************************************/
+
+/************* SIGILL *******************************/
+
+void causeSIGILLCrashF3();
+
+void causeSIGILLCrashF2();
+
+void causeSIGILLCrashF1();
+
+/***************************************************/
+
+/************* SIGBUS *******************************/
+
+void causeSIGBUSCrashF3();
+
+void causeSIGBUSCrashF2();
+
+void causeSIGBUSCrashF1();
+
+/***************************************************/
+
+/************* SIGTRAP *******************************/
+
+void causeSIGTRAPCrashF3();
+
+void causeSIGTRAPCrashF2();
+
+void causeSIGTRAPCrashF1();
+/***************************************************/
