374bfa0b13b395bbb805ec3a9f3bcdef680c9b04
Ahmed alaa <aalaa@instabug.com>
2024-02-27 00:04:16 +0200

    feat(example): add survey with token action

--- examples/default/src/screens/SurveysScreen.tsx
+++ examples/default/src/screens/SurveysScreen.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Surveys } from 'instabug-reactnative';
 
 import { ListTile } from '../components/ListTile';
 import { Screen } from '../components/Screen';
+import { VerticalListTile } from '../components/VerticalListTile';
+import { Button, Input, Text, useToast, VStack } from 'native-base';
+import { StyleSheet, View } from 'react-native';
 
 export const SurveysScreen: React.FC = () => {
+  const [surveyToken, setSurveyToken] = useState('');
+  const toast = useToast();
+  const [userAttributesFormError, setUserAttributesFormError] = useState<any>({});
+
+  const validateUserAttributeForm = () => {
+    const errors: any = {};
+    if (surveyToken.length === 0) {
+      errors.surveyToken = 'Value is required';
+    }
+
+    setUserAttributesFormError(errors);
+    return Object.keys(errors).length === 0;
+  };
+  const styles = StyleSheet.create({
+    inputWrapper: {
+      padding: 4,
+      flex: 1,
+    },
+    errorText: {
+      color: '#ff0000',
+    },
+    formContainer: {
+      flexDirection: 'row',
+      alignItems: 'stretch',
+    },
+  });
   return (
     <Screen>
       <ListTile
@@ -16,6 +45,46 @@ export const SurveysScreen: React.FC = () => {
         title="Show Custom Survey"
         onPress={() => Surveys.showSurvey('6ZaEI4nVdjg19r5uekS5nw')}
       />
+
+      <VerticalListTile title="Survey token actions">
+        <VStack>
+          <View style={styles.formContainer}>
+            <View style={styles.inputWrapper}>
+              <Input
+                placeholder="Survey token"
+                onChangeText={(key) => setSurveyToken(key)}
+                defaultValue={surveyToken}
+              />
+              {userAttributesFormError.surveyToken ? (
+                <Text style={styles.errorText}>{userAttributesFormError.surveyToken}</Text>
+              ) : null}
+            </View>
+          </View>
+
+          <Button
+            mt="4"
+            onPress={() => {
+              if (validateUserAttributeForm()) {
+                Surveys.showSurvey(surveyToken);
+              }
+            }}>
+            Show survey
+          </Button>
+
+          <Button
+            mt="4"
+            onPress={async () => {
+              if (validateUserAttributeForm()) {
+                const hasResponded = await Surveys.hasRespondedToSurvey(surveyToken);
+                toast.show({
+                  description: hasResponded ? 'YES' : 'NO',
+                });
+              }
+            }}>
+            If User has responded survey
+          </Button>
+        </VStack>
+      </VerticalListTile>
     </Screen>
   );
 };
