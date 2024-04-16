019f5b5d521d5e30066ee6582932cb8a410e8738
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-04-16 18:16:49 +0200

    refactor: allow replace script to replace in multiple files

--- scripts/replace.js
+++ scripts/replace.js
@@ -11,30 +11,32 @@
 const fs = require('fs');
 const path = require('path');
 
-const [search, replace, file] = process.argv.slice(2);
+const [search, replace, ...files] = process.argv.slice(2);
 
-if (!search || !replace || !file) {
+if (!search || !replace || !files.length) {
   // The path of the script relative to the directory where the user ran the
   // script to be used in the error message demonstrating the usage.
   const scriptPath = path.relative(process.cwd(), __filename);
 
   console.error('Missing arguments.');
-  console.table({ search, replace, file });
+  console.table({ search, replace, files });
 
-  console.error(`Usage: node ${scriptPath} <search> <replace> <file>`);
+  console.error(`Usage: node ${scriptPath} <search> <replace> <files...>`);
   process.exit(1);
 }
 
-try {
-  const filePath = path.resolve(process.cwd(), file);
+for (const file of files) {
+  try {
+    const filePath = path.resolve(process.cwd(), file);
 
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+    const fileContent = fs.readFileSync(filePath, 'utf8');
 
-  const newContent = fileContent.replaceAll(search, replace);
+    const newContent = fileContent.replaceAll(search, replace);
 
-  fs.writeFileSync(filePath, newContent);
-} catch (error) {
-  console.error('An error occurred while replacing the content of the file.');
-  console.error(error);
-  process.exit(1);
+    fs.writeFileSync(filePath, newContent);
+  } catch (error) {
+    console.error(`An error occurred while replacing the content of the file: ${file}.`);
+    console.error(error);
+    process.exit(1);
+  }
 }
