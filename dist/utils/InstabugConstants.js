b626256bc24e43a44f0b6520db2f7290f0e20734
Instabug-CP-CI <github-ci+cp@instabug.com>
2024-05-13 14:54:15 +0000

    chore: add generate files

---
+++ dist/utils/InstabugConstants.js
@@ -0,0 +1,5 @@
+var InstabugConstants;
+(function (InstabugConstants) {
+    InstabugConstants["GRAPHQL_HEADER"] = "ibg-graphql-header";
+})(InstabugConstants || (InstabugConstants = {}));
+export default InstabugConstants;
