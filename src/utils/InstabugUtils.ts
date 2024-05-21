b8c28726a4412e15fbc63d429e31cf1b83dbedec
kholood <keassa@instabug.com>
2024-05-21 14:26:27 +0300

    ci:fix lint

--- src/utils/InstabugUtils.ts
+++ src/utils/InstabugUtils.ts
@@ -130,29 +130,31 @@ export async function sendCrashReport(
  * Generate random 32 bit unsigned integer Hexadecimal (8 chars) lower case letters
  * Should not return all zeros
  */
-export const generateTracePartialId=()=> {
+export const generateTracePartialId = () => {
   let hexString;
   do {
-      let randomNumber = Math.floor(Math.random() * Math.pow(2, 32));
-      hexString = randomNumber.toString(16).toLocaleLowerCase(); 
-      while (hexString.length < 8) {
-          hexString = '0' + hexString;
-      }
-  } while (hexString === '00000000'); 
-  
+    const randomNumber = Math.floor(Math.random() * Math.pow(2, 32));
+    hexString = randomNumber.toString(16).toLocaleLowerCase();
+    while (hexString.length < 8) {
+      hexString = '0' + hexString;
+    }
+  } while (hexString === '00000000');
+
   return hexString;
-}
+};
 /**
- * Generate W3C header 
- * @param networkStartTime 
+ * Generate W3C header
+ * @param networkStartTime
  * @returns w3c header
  */
-export const generateW3CHeader=(networkStartTime:number)=>{
-  const hexaDigitsTimestamp=(Math.floor(networkStartTime.valueOf()/1000)).toString(16).toLowerCase();
-  const partialId=generateTracePartialId();
-  
-  return `${hexaDigitsTimestamp}${partialId}${hexaDigitsTimestamp}${partialId}`
-}
+export const generateW3CHeader = (networkStartTime: number) => {
+  const hexaDigitsTimestamp = Math.floor(networkStartTime.valueOf() / 1000)
+    .toString(16)
+    .toLowerCase();
+  const partialId = generateTracePartialId();
+
+  return `${hexaDigitsTimestamp}${partialId}${hexaDigitsTimestamp}${partialId}`;
+};
 
 export function isContentTypeNotAllowed(contentType: string) {
   const allowed = [
@@ -232,5 +234,5 @@ export default {
   stringifyIfNotString,
   sendCrashReport,
   generateTracePartialId,
-  generateW3CHeader
+  generateW3CHeader,
 };
