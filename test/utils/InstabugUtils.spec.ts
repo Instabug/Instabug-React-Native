be7e75c3cb01d5dce10f531d63e5e8377167b541
kholood <keassa@instabug.com>
2024-05-22 17:15:08 +0300

    feat:update header format test case title

--- test/utils/InstabugUtils.spec.ts
+++ test/utils/InstabugUtils.spec.ts
@@ -351,7 +351,7 @@ describe('reportNetworkLog', () => {
     mockMathRandom.mockRestore();
   });
 
-  it('generateW3CHeader should return timestampidtimestampid format header', () => {
+  it('generateW3CHeader should return {version}-{trace-id}-{parent-id}-{trace-flag} format header', () => {
     const mockMathRandom = jest.spyOn(global.Math, 'random');
 
     mockMathRandom.mockReturnValueOnce(0).mockReturnValueOnce(0.5).mockReturnValueOnce(1);
