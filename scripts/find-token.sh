b25a310a3d3cd864d00097b29b7361bc2cff43c0
Ahmed alaa <aalaa@instabug.com>
2024-05-28 19:56:32 +0300

    feat: read INSTABUG_APP_TOKEN from env file in automatic sourcemap file upload

--- scripts/find-token.sh
+++ scripts/find-token.sh
@@ -8,7 +8,7 @@ INIT_APP_TOKEN=$(
     grep -o "[\"\'][0-9a-zA-Z]*[\"\']" |
     cut -d "\"" -f 2 |
     cut -d "'" -f 2
-) 
+)
 
 if [ ! -z "${INIT_APP_TOKEN}" ]; then
     echo $INIT_APP_TOKEN
@@ -20,10 +20,26 @@ START_APP_TOKEN=$(
     grep -o "[\"\'][0-9a-zA-Z]*[\"\']" |
     cut -d "\"" -f 2 |
     cut -d "'" -f 2
-) 
+)
 
 if [ ! -z "${START_APP_TOKEN}" ]; then
     echo $START_APP_TOKEN
+    exit 0
+fi
+
+
+
+
+ENV_APP_TOKEN=$(
+    grep "INSTABUG_APP_TOKEN" -r -A 1 -m 1 --exclude-dir={node_modules,ios,android} --include=\*.env ./ |
+    sed 's/ //g' |
+    grep -o "=[0-9a-zA-Z]*" |
+    cut -d "=" -f 2
+
+     )
+
+if [ ! -z "${ENV_APP_TOKEN}" ]; then
+    echo $ENV_APP_TOKEN
     exit 0
 fi
 
