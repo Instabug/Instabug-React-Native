1c96a4a0f63218cc1cc2b948a3c0c78eaccd99aa
kholood <keassa@instabug.com>
2024-06-06 10:24:22 +0300

    ci: fix lint

--- examples/default/src/screens/apm/APMScreen.tsx
+++ examples/default/src/screens/apm/APMScreen.tsx
@@ -1,21 +1,21 @@
+import React from 'react';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import axios from 'axios';
 
 import type { HomeStackParamList } from '../../navigation/HomeStack';
-import React from 'react';
 import { ListTile } from '../../components/ListTile';
 import { Screen } from '../../components/Screen';
 
 export const APMScreen: React.FC<NativeStackScreenProps<HomeStackParamList, 'APM'>> = ({
   navigation,
 }) => {
-  const simulateNetworkRequest = async () => {
-    const response = await axios.get('https://httpbin.org/anything', {
+  const simulateNetworkRequest = () => {
+    axios.get('https://httpbin.org/anything', {
       headers: { traceparent: 'Caught Header Example' },
     });
   };
-  const simulateNetworkRequestWithoutHeader = async () => {
-    const response = await axios.get('https://httpbin.org/anything');
+  const simulateNetworkRequestWithoutHeader = () => {
+    axios.get('https://httpbin.org/anything');
   };
   return (
     <Screen>
