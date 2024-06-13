513c19e75de663611c424fa73ac0735fa3d8ba41
Ahmed alaa <aalaa@instabug.com>
2024-06-13 19:01:57 +0300

    feat: control sending screenshot with each crash type

--- examples/default/src/App.tsx
+++ examples/default/src/App.tsx
@@ -1,29 +1,24 @@
-import React, { useEffect } from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import Instabug, {
-  CrashReporting,
-  InvocationEvent,
-  LogLevel,
-  ReproStepsMode
-} from "instabug-reactnative";
-import { NativeBaseProvider } from "native-base";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import Instabug, { InvocationEvent, LogLevel, ReproStepsMode } from 'instabug-reactnative';
+import { NativeBaseProvider } from 'native-base';
 
-import { RootTabNavigator } from "./navigation/RootTab";
-import { nativeBaseTheme } from "./theme/nativeBaseTheme";
-import { navigationTheme } from "./theme/navigationTheme";
+import { RootTabNavigator } from './navigation/RootTab';
+import { nativeBaseTheme } from './theme/nativeBaseTheme';
+import { navigationTheme } from './theme/navigationTheme';
 
 export const App: React.FC = () => {
   useEffect(() => {
     Instabug.init({
-      token: "deb1910a7342814af4e4c9210c786f35",
+      token: 'deb1910a7342814af4e4c9210c786f35',
       invocationEvents: [InvocationEvent.floatingButton],
-      debugLogsLevel: LogLevel.verbose
+      debugLogsLevel: LogLevel.verbose,
     });
     Instabug.setReproStepsConfig({
-      outOfMemory: ReproStepsMode.enabled
+      outOfMemory: ReproStepsMode.enabled,
     });
   }, []);
 
@@ -40,6 +35,6 @@ export const App: React.FC = () => {
 
 const styles = StyleSheet.create({
   root: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
