e2cd1d6851cc2cb26bce5564a1fa37ae6e04bb0f
Ahmed Elrefaey <68241710+a7medev@users.noreply.github.com>
2024-07-18 15:58:56 +0300

    fix(android): resolve an OOM in network logs (#1244)

--- ios/RNInstabug/Util/IBGNetworkLogger+CP.h
+++ ios/RNInstabug/Util/IBGNetworkLogger+CP.h
@@ -5,6 +5,22 @@ NS_ASSUME_NONNULL_BEGIN
 @interface IBGNetworkLogger (CP)
 
 + (void)disableAutomaticCapturingOfNetworkLogs;
++ (void)addNetworkLogWithUrl:(NSString *)url
+                      method:(NSString *)method
+                 requestBody:(NSString *)request
+             requestBodySize:(int64_t)requestBodySize
+                responseBody:(NSString *)response
+            responseBodySize:(int64_t)responseBodySize
+                responseCode:(int32_t)code
+              requestHeaders:(NSDictionary *)requestHeaders
+             responseHeaders:(NSDictionary *)responseHeaders
+                 contentType:(NSString *)contentType
+                 errorDomain:(NSString *)errorDomain
+                   errorCode:(int32_t)errorCode
+                   startTime:(int64_t)startTime
+                    duration:(int64_t) duration
+                gqlQueryName:(NSString * _Nullable)gqlQueryName
+          serverErrorMessage:(NSString * _Nullable)serverErrorMessage;
 
 @end
 
