604a65457bd104f8c19e6dd8e215ec43134add84
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-08-13 16:21:34 +0300

    chore: make dream11 script macos-compatible

--- scripts/dream-11-delete-unused-features.sh
+++ scripts/dream-11-delete-unused-features.sh
@@ -4,10 +4,12 @@
 deletedFeaturesFilesInJavaScript=("Surveys" "FeatureRequests" "Survey")
 for feature in "${deletedFeaturesFilesInJavaScript[@]}"; do
 	echo "$feature"
+
 	rm -f src/modules/"$feature".ts
 	rm -f test/mocks/mock"$feature".ts
-	sed -i "s/import..*$feature';//g" src/index.ts
-	sed -i "s/$feature,//g" src/index.ts
+
+	node scripts/replace.js --pattern "import.+$feature';" "" src/index.ts
+	node scripts/replace.js --pattern "$feature," "" src/index.ts
 done
 
 npx eslint src/index.ts --fix
@@ -19,7 +21,7 @@ for feature in "${deletedFeaturesFilesInAndroidApp[@]}"; do
 
 	rm -f android/src/main/java/com/instabug/reactlibrary/"$feature".java
 	rm -f android/src/test/java/com/instabug/reactlibrary/"$feature"Test.java
-	sed -i "s/modules.add(new $feature(reactContext));//g" android/src/main/java/com/instabug/reactlibrary/RNInstabugReactnativePackage.java
+	node scripts/replace.js "modules.add(new $feature(reactContext));" "" android/src/main/java/com/instabug/reactlibrary/RNInstabugReactnativePackage.java
 done
 
 # remove survey and featureRequest features in IOS  files
@@ -30,13 +32,15 @@ for feature in "${deletedFeaturesFilesInIosApp[@]}"; do
 	rm -f ios/RNInstabug/"$feature".m
 done
 
-sed -i "s/\#import <Instabug\/IBGSurveys.h>//g" ios/RNInstabug/InstabugReactBridge.m
-sed -i "s/\#import <Instabug\/IBGSurveys.h>//g" ios/RNInstabug/InstabugReactBridge.h
+node scripts/replace.js "#import <Instabug/IBGSurveys.h>" "" ios/RNInstabug/InstabugReactBridge.m
+node scripts/replace.js "#import <Instabug/IBGSurveys.h>" "" ios/RNInstabug/InstabugReactBridge.h
 
-# remove all locales except English locale
-sed -i -E '/english/!s/.*constants.locale.*//g' src/utils/Enums.ts
+# Remove all locales except for English
+# This ugly regular expression matches all lines not containing "english" and containing "constants.locale"
+node scripts/replace.js --pattern "^(?!.*english).+constants\.locale.*" "" src/utils/Enums.ts
 npx eslint src/index.ts --fix src/utils/Enums.ts
 
-sed -i "s/return (major == 7 && minor >= 3) || major >= 8/return false/g" android/build.gradle
+node scripts/replace.js "return (major == 7 && minor >= 3) || major >= 8" "return false" android/build.gradle
 
-sed -i "s/static boolean supportsNamespace() {/static boolean supportsNamespace() { \n return false/g" android/build.gradle
+# Note: printf is used here as the string contains a newline character which would be escaped otherwise.
+node scripts/replace.js "static boolean supportsNamespace() {" "$(printf "static boolean supportsNamespace() {\n  return false")" android/build.gradle
