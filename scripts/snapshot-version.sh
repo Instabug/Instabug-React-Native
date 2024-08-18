98c67fda8c2ded4a585d3fb4d09583b19beff32a
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-08-18 16:28:23 +0300

    chore: use CIRCLE_PULL_REQUEST to get PR number

--- scripts/snapshot-version.sh
+++ scripts/snapshot-version.sh
@@ -3,11 +3,9 @@
 # Generates a snapshot version following the format {version}-{pr}{random-3-digit}-SNAPSHOT
 # Example: 13.3.0-502861-SNAPSHOT
 
-pr=$CIRCLE_PR_NUMBER
+pr=$(basename $CIRCLE_PULL_REQUEST)
 random=$(($RANDOM % 900 + 100))
 version=$(jq -r '.version' package.json)
 suffix="SNAPSHOT"
 
 SNAPSHOT_VERSION="$version-$pr$random-$suffix"
-
-echo $SNAPSHOT_VERSION
