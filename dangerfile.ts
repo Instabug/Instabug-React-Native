2bffe839501827d8c921549766553c7683ed11ee
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-09-30 09:29:42 +0300

    chore: add ndk package

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,21 +1,28 @@
 import collectCoverage, { ReportType } from '@instabug/danger-plugin-coverage';
 import { danger, fail, schedule, warn } from 'danger';
 
-const hasSourceChanges = danger.git.modified_files.some((file) => file.startsWith('src/'));
+const hasSourceChanges = danger.git.modified_files.some((file) =>
+  file.startsWith('src/')
+);
 const declaredTrivial =
-  !hasSourceChanges || danger.github.issue.labels.some((label) => label.name === 'trivial');
+  !hasSourceChanges ||
+  danger.github.issue.labels.some((label) => label.name === 'trivial');
 
 // Make sure PR has a description.
 async function hasDescription() {
   const linesOfCode = (await danger.git.linesOfCode()) ?? 0;
-  const hasNoDiscription = danger.github.pr.body.includes('> Description goes here');
+  const hasNoDiscription = danger.github.pr.body.includes(
+    '> Description goes here'
+  );
   if (hasNoDiscription && linesOfCode > 10) {
-    fail('Please provide a summary of the changes in the pull request description.');
+    fail(
+      'Please provide a summary of the changes in the pull request description.'
+    );
   }
 
   if (!danger.git.modified_files.includes('CHANGELOG.md') && !declaredTrivial) {
     warn(
-      'You have not included a CHANGELOG entry! \nYou can find it at [CHANGELOG.md](https://github.com/Instabug/Instabug-React-Native/blob/master/CHANGELOG.md).',
+      'You have not included a CHANGELOG entry! \nYou can find it at [CHANGELOG.md](https://github.com/Instabug/Instabug-React-Native/blob/master/CHANGELOG.md).'
     );
   }
 }
