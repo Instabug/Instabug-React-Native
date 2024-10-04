cfcd55b1bb99e1769410af36fb38bfb8665cacd3
kholood <keassa@instabug.com>
2024-10-04 12:30:45 +0300

    fix(android): convert session metadat map to readable map

--- android/src/main/java/com/instabug/reactlibrary/RNInstabugSessionReplayModule.java
+++ android/src/main/java/com/instabug/reactlibrary/RNInstabugSessionReplayModule.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.instabug.library.OnSessionReplayLinkReady;
@@ -119,7 +120,7 @@ public class RNInstabugSessionReplayModule extends EventEmitterModule {
 
     }
 
-    public WritableMap getSessionMetadataMap(SessionMetadata sessionMetadata){
+    public ReadableMap getSessionMetadataMap(SessionMetadata sessionMetadata){
         WritableMap params = Arguments.createMap();
         params.putString("appVersion",sessionMetadata.getAppVersion());
         params.putString("OS",sessionMetadata.getOs());
