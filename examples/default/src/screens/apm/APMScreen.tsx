c2faf991ed943dd6f8f0abc1cd42104993f8fed4
YoussefFouadd <yfouad@instabug.com>
2024-10-06 17:05:34 +0300

    Resolving comments

--- examples/default/src/screens/apm/APMScreen.tsx
+++ examples/default/src/screens/apm/APMScreen.tsx
@@ -3,8 +3,9 @@ import type { HomeStackParamList } from '../../navigation/HomeStack';
 import React, { useState } from 'react';
 import { ListTile } from '../../components/ListTile';
 import { Screen } from '../../components/Screen';
-import { Text, Switch, Alert, ToastAndroid, Platform } from 'react-native';
+import { Text, Switch } from 'react-native';
 import { APM } from 'instabug-reactnative';
+import { showNotification } from '../../utils/showNotification';
 
 export const APMScreen: React.FC<NativeStackScreenProps<HomeStackParamList, 'APM'>> = ({
   navigation,
@@ -13,28 +14,14 @@ export const APMScreen: React.FC<NativeStackScreenProps<HomeStackParamList, 'APM
 
   const toggleSwitch = (value: boolean) => {
     setIsEnabled(value);
-
-    // Update APM state here
     APM.setEnabled(value);
-
-    // Show Toast message
-
-    if (Platform.OS === 'android') {
-      ToastAndroid.show('Set APM enabled to ' + value, ToastAndroid.SHORT);
-    } else {
-      Alert.alert('Set APM enabled to ' + value);
-    }
+    showNotification('APM status', 'APM enabled set to ' + value);
   };
 
   return (
     <Screen>
       <Text>Enable APM:</Text>
       <Switch onValueChange={toggleSwitch} value={isEnabled} />
-      {/* <Text>Enable Cold Launch:</Text>
-      <Switch
-        onValueChange={toggleSwitch2}
-        value={isEnabled2}
-      /> */}
       <ListTile title="End App launch" onPress={() => APM.endAppLaunch()} />
       <ListTile title="Network Screen" onPress={() => navigation.navigate('NetworkTraces')} />
       <ListTile title="Traces" onPress={() => navigation.navigate('ExecutionTraces')} />
