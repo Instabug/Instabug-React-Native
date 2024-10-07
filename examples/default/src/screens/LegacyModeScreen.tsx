3fb25a34d73620fa6ded36d899026eec51ec26de
YoussefFouadd <yfouad@instabug.com>
2024-10-07 19:22:08 +0300

    Resolving latest 2 comments

--- examples/default/src/screens/LegacyModeScreen.tsx
+++ examples/default/src/screens/LegacyModeScreen.tsx
@@ -8,40 +8,12 @@ import { showNotification } from '../utils/showNotification';
 export const LegacyModeScreen: React.FC = () => {
   const [loading, setLoading] = useState(false);
 
-  const addInstabugLog = (log: string): Promise<void> => {
-    return new Promise<void>((resolve) => {
-      Instabug.logDebug(log);
-      resolve();
-    });
-  };
-
-  const addUserEvent = (event: string): Promise<void> => {
-    return new Promise<void>((resolve) => {
-      Instabug.logUserEvent(event);
-      resolve();
-    });
-  };
-  const addTag = (tag: string): Promise<void> => {
-    return new Promise<void>((resolve) => {
-      Instabug.appendTags([tag]);
-      resolve();
-    });
-  };
-  const addUserAttributes = (key: string, value: string): Promise<void> => {
-    return new Promise<void>((resolve) => {
-      Instabug.setUserAttribute(key, value);
-      resolve();
-    });
-  };
-
   const addMultipleInstabugLogs = async (numberOfLogs: number) => {
     setLoading(true);
     try {
-      const logPromises: Promise<void>[] = [];
       for (let i = 0; i < numberOfLogs; i++) {
-        logPromises.push(addInstabugLog(`log ${i}`));
+        Instabug.logDebug(`log ${i}`);
       }
-      await Promise.all(logPromises);
       showNotification('Success', 'Succeeded');
     } catch (error) {
       showNotification('Error', 'Failed');
@@ -53,11 +25,9 @@ export const LegacyModeScreen: React.FC = () => {
   const addMultipleUserEvents = async (numberOfLogs: number) => {
     setLoading(true);
     try {
-      const logPromises: Promise<void>[] = [];
       for (let i = 0; i < numberOfLogs; i++) {
-        logPromises.push(addUserEvent(`test user event ${i}`));
+        Instabug.logUserEvent(`test user event ${i}`);
       }
-      await Promise.all(logPromises);
       showNotification('Success', 'Succeeded');
     } catch (error) {
       showNotification('Error', 'Failed');
@@ -69,11 +39,9 @@ export const LegacyModeScreen: React.FC = () => {
   const addMultipleTags = async (numberOfLogs: number) => {
     setLoading(true);
     try {
-      const logPromises: Promise<void>[] = [];
       for (let i = 0; i < numberOfLogs; i++) {
-        logPromises.push(addTag(`test tag ${i}`));
+        Instabug.appendTags([`test tag ${i}`]);
       }
-      await Promise.all(logPromises);
       showNotification('Success', 'Succeeded');
     } catch (error) {
       showNotification('Error', 'Failed');
@@ -85,11 +53,9 @@ export const LegacyModeScreen: React.FC = () => {
   const addMultipleUserAttributes = async (numberOfLogs: number) => {
     setLoading(true);
     try {
-      const logPromises: Promise<void>[] = [];
       for (let i = 0; i < numberOfLogs; i++) {
-        logPromises.push(addUserAttributes(`user${i}`, `user${i} value`));
+        Instabug.setUserAttribute(`user${i}`, `user${i} value`);
       }
-      await Promise.all(logPromises);
       showNotification('Success', 'Succeeded');
     } catch (error) {
       showNotification('Error', 'Failed');
