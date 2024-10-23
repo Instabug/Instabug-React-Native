66defd87cda272b3e813bb9bb6bba696c090ea8c
AyaMahmoud148 <ayaabdelbaseet@instabug.com>
2024-10-23 11:12:36 +0300

    feat: add hybrid android example app

---
+++ examples/hybridexampleapp/android/app/src/main/java/com/instabug/hybridsampleapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.instabug.hybridsampleapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.instabug.crash.CrashReporting;
+import com.instabug.crash.models.IBGNonFatalException;
+import com.instabug.hybridsampleapp.databinding.ActivityMainBinding;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonReactNative.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, ReactNativeAppActivity.class);
+            startActivity(intent);
+        });
+
+
+        binding.buttonHandled.setOnClickListener(view -> {
+            Map<String, String> attributes = new HashMap<>();
+            attributes.put("hello", "world");
+
+            IBGNonFatalException exception = new IBGNonFatalException.Builder(new Exception("Handled Java Crash"))
+                    .setUserAttributes(attributes)
+                    .setFingerprint("My Custom Fingerprint")
+                    .setLevel(IBGNonFatalException.Level.CRITICAL)
+                    .build();
+            CrashReporting.report(exception);
+        });
+
+        binding.buttonUnhandled.setOnClickListener(view -> {
+            throw new Error("Unhandled Java Crash");
+        });
+    }
+}
---
+++ examples/hybridexampleapp/android/app/src/main/java/com/instabug/hybridsampleapp/ReactNativeAppActivity.java
@@ -0,0 +1,86 @@
+package com.instabug.hybridsampleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.soloader.SoLoader;
+import java.util.List;
+import com.instabug.hybridsampleapp.BuildConfig;  // Ensure you have the correct package for BuildConfig
+
+public class ReactNativeAppActivity extends Activity implements DefaultHardwareBackBtnHandler {
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SoLoader.init(this, false);
+
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(new MyReactNativePackage());
+        // Remember to include them in `settings.gradle` and `app/build.gradle` too.
+        List<ReactPackage> packages = new PackageList(getApplication()).getPackages();
+
+        mReactRootView = new ReactRootView(this);
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setCurrentActivity(this)
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index")
+                .addPackages(packages)
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        // The string here (e.g. "MyReactNativeApp") has to match
+        // the string in AppRegistry.registerComponent() in index.js
+        mReactRootView.startReactApplication(mReactInstanceManager, "HybridApp", null);
+        setContentView(mReactRootView);
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this);  // Corrected the method call
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy(this);
+        }
+        if (mReactRootView != null) {
+            mReactRootView.unmountReactApplication();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
