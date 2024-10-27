3f6bcf8991d9f3984a5b34b4391acffa5b14eedd
AyaMahmoud148 <ayaabdelbaseet@instabug.com>
2024-10-27 14:15:17 +0300

    fix: use RNInstabug instead of native SDK

--- examples/hybridexampleapp/android/app/src/main/java/com/instabug/hybridsampleapp/HybridApplication.java
+++ examples/hybridexampleapp/android/app/src/main/java/com/instabug/hybridsampleapp/HybridApplication.java
@@ -1,8 +1,8 @@
 package com.instabug.hybridsampleapp;
+import com.instabug.reactlibrary.RNInstabug;
 
 import android.app.Application;
 import android.util.Log;
-import com.instabug.library.Instabug;
 import com.instabug.library.LogLevel;
 import com.instabug.library.invocation.InstabugInvocationEvent;
 
@@ -12,8 +12,7 @@ public class HybridApplication extends Application {
         super.onCreate();
         Log.v("Instabug-Hybrid", "starting from native");
 
-        new Instabug.Builder(this, "deb1910a7342814af4e4c9210c786f35")
-                .setSdkDebugLogsLevel(LogLevel.VERBOSE)
+        new RNInstabug.Builder(this, "deb1910a7342814af4e4c9210c786f35")
                 .setInvocationEvents(InstabugInvocationEvent.SHAKE, InstabugInvocationEvent.FLOATING_BUTTON)
                 .build();
     }
