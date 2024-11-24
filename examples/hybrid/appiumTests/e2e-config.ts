421f4e26217607fd409e309ee01b1d1a54aacdef
AyaMahmoud148 <ayaabdelbaseet@instabug.com>
2024-11-24 20:25:30 +0200

    fix: add run command

--- examples/hybrid/appiumTests/e2e-config.ts
+++ examples/hybrid/appiumTests/e2e-config.ts
@@ -7,7 +7,7 @@ const android = {
   'appium:platformVersion': '14', //Update Your Platform Version
   'appium:deviceName': 'pixel_3a',
   'appium:avd': 'Pixel_8',
-  'appium:app': './android/app/build/outputs/apk/debug/app-debug.apk', //apk path
+  'appium:app': './android/app/build/outputs/apk/release/app-release.apk', //apk path
   'appium:appPackage': 'com.instabug.hybridsampleapp',
   'appium:appActivity': '.MainActivity',
   'appium:automationName': 'UiAutomator2',
