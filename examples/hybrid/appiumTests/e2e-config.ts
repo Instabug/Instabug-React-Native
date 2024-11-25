2cffab09e15c9f7e737cb58679cd358b9be5e027
AyaMahmoud148 <ayaabdelbaseet@instabug.com>
2024-11-25 11:17:36 +0200

    fix: set emulator wiifi

--- examples/hybrid/appiumTests/e2e-config.ts
+++ examples/hybrid/appiumTests/e2e-config.ts
@@ -7,10 +7,15 @@ const android = {
   'appium:platformVersion': '14', //Update Your Platform Version
   'appium:deviceName': 'pixel_3a',
   'appium:avd': 'Pixel_8',
-  'appium:app': process.env.APK_PATH || './android/app/build/outputs/apk/release/app-release.apk', //apk path
+  'appium:app': './android/app/build/outputs/apk/release/app-release.apk', //apk path
   'appium:appPackage': 'com.instabug.hybridsampleapp',
   'appium:appActivity': '.MainActivity',
   'appium:automationName': 'UiAutomator2',
+  'appium:proxy': {
+    proxyType: 'manual',
+    httpProxy: 'localhost:8082',
+    sslProxy: 'localhost:8082',
+  },
 };
 
 const ios = {
