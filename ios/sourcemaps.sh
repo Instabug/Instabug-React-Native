f0fce288e865ad5f39311b638408489509f8bf1d
Ahmed alaa <aalaa@instabug.com>
2024-11-25 16:52:14 +0200

    fix: ios build on EAS

--- ios/sourcemaps.sh
+++ ios/sourcemaps.sh
@@ -41,12 +41,14 @@ main() {
   local inferred_code=$(/usr/libexec/PlistBuddy -c 'print CFBundleVersion' "$PROJECT_DIR/$INFOPLIST_FILE")
   local version_code=$(resolve_var "Version Code" "INSTABUG_APP_VERSION_CODE" "$inferred_code" | tail -n 1)
 
+  if [ -z "$source_map_file" ]; then
   node $instabug_dir/bin/index.js upload-sourcemaps \
       --platform ios \
       --file $source_map_file \
       --token $app_token \
       --name $version_name \
       --code $version_code
+  fi
 }
 
 generate_sourcemaps() {
---
+++ plugins/withIosSourcemaps.js
@@ -0,0 +1,42 @@
+const { withXcodeProject } = require('@expo/config-plugins');
+
+const BUILD_PHASE = 'PBXShellScriptBuildPhase';
+
+function withIosPlugin(config) {
+  return withXcodeProject(config, (config) => {
+    const xcodeProject = config.modResults;
+
+    const bundleReactNativePhase = xcodeProject.pbxItemByComment(
+      'Bundle React Native code and images',
+      BUILD_PHASE
+    );
+
+    bundleReactNativePhase.shellScript = addSourceMapExport(
+      bundleReactNativePhase.shellScript
+    );
+
+    const uploadSourcemapPhase = xcodeProject.pbxItemByComment(
+      '[CP-User] [instabug-reactnative] Upload Sourcemap',
+      BUILD_PHASE
+    );
+
+    uploadSourcemapPhase.shellScript = addSourceMapExport(
+      uploadSourcemapPhase.shellScript
+    );
+
+    return config;
+  });
+}
+
+function addSourceMapExport(script) {
+  const sourceMapFileExport =
+    '"export SOURCEMAP_FILE=\\"$TMPDIR/$(md5 -qs \\"$CONFIGURATION_BUILD_DIR\\")-main.jsbundle.map\\"\\n';
+
+  if (!script.includes(sourceMapFileExport)) {
+    return sourceMapFileExport + script.substring(1);
+  }
+
+  return script;
+}
+
+module.exports = withIosPlugin;
