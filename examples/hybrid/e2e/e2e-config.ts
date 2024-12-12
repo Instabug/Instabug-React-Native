0fe4c998a67461e2d64bf41d5fea58c5edcb4961
AyaMahmoud148 <ayaabdelbaseet@instabug.com>
2024-12-12 09:21:09 +0200

    fix: try ci

--- examples/hybrid/e2e/e2e-config.ts
+++ examples/hybrid/e2e/e2e-config.ts
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 export let capabilities: any;
 
 const android = {
   platformName: 'Android',
   'appium:platformVersion': '14', //Update Your Platform Version
-  'appium:deviceName': 'Pixel_8',
+  'appium:deviceName': 'pixel_3a',
   'appium:avd': 'Pixel_8',
   'appium:app': process.env.APK_PATH || './android/app/build/outputs/apk/release/app-release.apk', //apk path
   'appium:appPackage': 'com.instabug.hybridsampleapp',
