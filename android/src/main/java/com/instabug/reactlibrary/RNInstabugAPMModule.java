252126e0d47039cb5cdbd1c45c8db530c8cb8fd6
Andrew Amin <adaniel@instabug.com>
2025-02-17 16:31:09 +0200

    chore: enhance inline code documentation for APM module.

--- android/src/main/java/com/instabug/reactlibrary/RNInstabugAPMModule.java
+++ android/src/main/java/com/instabug/reactlibrary/RNInstabugAPMModule.java
@@ -43,7 +43,9 @@ public class RNInstabugAPMModule extends EventEmitterModule {
     }
 
     /**
-     * Sets the printed logs priority. Filter to one of the following levels.
+     * This method is exporting a method
+     * named `ibgSleep` to be accessible from JavaScript side. When this method is
+     * called from JavaScript, it (sleeps) the current thread for 3 seconds.
      */
     @ReactMethod
     public void ibgSleep() {
@@ -57,6 +59,14 @@ public class RNInstabugAPMModule extends EventEmitterModule {
 
     /**
      * Enables or disables APM.
+     *
+     * This function is exporting a method named
+     * [setEnabled] to be accessible from JavaScript side. When this method is
+     * called from JavaScript, it will set the `enabled` property of the [APM] class provided by the
+     * Instabug SDK to the value passed as the `isEnabled` parameter. This property controls whether the
+     * APM (Application Performance Monitoring) feature of Instabug is enabled or disabled based on the
+     * boolean value passed to it.
+     *
      * @param isEnabled boolean indicating enabled or disabled.
      */
     @ReactMethod
@@ -75,6 +85,12 @@ public class RNInstabugAPMModule extends EventEmitterModule {
 
     /**
      * Enables or disables app launch tracking.
+     *
+     * This function is exporting a method named
+     * `setAppLaunchEnabled` to be accessible from JavaScript side. When this
+     * method is called from JavaScript, it will set the `coldAppLaunchEnabled` property of the `IBGAPM`
+     * class provided by the Instabug SDK to the value passed as the `isEnabled` parameter.
+     *
      * @param isEnabled boolean indicating enabled or disabled.
      */
     @ReactMethod
@@ -92,7 +108,10 @@ public class RNInstabugAPMModule extends EventEmitterModule {
     }
 
     /**
-     * Ends app launch
+     * This function is exporting a method named `endAppLaunch` to be
+     * accessible from JavaScript side. When this method is called from
+     * JavaScript, it will invoke the `endAppLaunch` method from the [APM] class provided by the
+     * Instabug SDK. This method is used to signal the end of the app launch process.
      */
     @ReactMethod
     public void endAppLaunch() {
@@ -110,6 +129,15 @@ public class RNInstabugAPMModule extends EventEmitterModule {
 
     /**
      * Enables or disables auto UI tracing
+     *
+     * This function is exporting a method named
+     * [setAutoUITraceEnabled] to be accessible from JavaScript side. When this
+     * method is called from JavaScript, it will set the `autoUITraceEnabled` property of the [APM]
+     * class provided by the Instabug SDK to the value passed as the `isEnabled` parameter. This property
+     * controls whether automatic tracing of UI interactions is enabled or disabled within the SDK. By
+     * toggling this property, you can control whether the SDK captures data related to user interface
+     * performance and behavior automatically.
+     *
      * @param isEnabled boolean indicating enabled or disabled.
      */
     @ReactMethod
@@ -281,6 +309,12 @@ public class RNInstabugAPMModule extends EventEmitterModule {
 
     /**
      * Starts a UI trace
+     *
+     * This function is exporting a method named
+     * [startUITrace] to be accessible from JavaScript side. When this method is
+     * called from JavaScript, it will invoke the [startUITrace] method from the [APM] class
+     * provided by the Instabug SDK to start a new UI trace.
+     *
      * @param name string name of the UI trace.
      */
     @ReactMethod
@@ -298,7 +332,10 @@ public class RNInstabugAPMModule extends EventEmitterModule {
     }
 
     /**
-     * Ends the current running UI trace
+     * This function is exporting a method named [endUITrace] to be
+     * accessible from JavaScript side. When this method is called from
+     * JavaScript, it will invoke the `endUITrace` method from the []APM] class provided by the Instabug
+     * SDK. This method is used to terminate the currently active UI trace.
      */
     @ReactMethod
     public void endUITrace() {
@@ -314,6 +351,73 @@ public class RNInstabugAPMModule extends EventEmitterModule {
         });
     }
 
+ /**
+  * The `networkLogAndroid` function logs network-related information using APMNetworkLogger in a React
+  * Native module.
+  *
+  * @param requestStartTime The `requestStartTime` parameter in the `networkLogAndroid` method
+  * represents the timestamp when the network request started. It is of type `double` and is passed as
+  * a parameter to log network-related information.
+  * @param requestDuration The `requestDuration` parameter in the `networkLogAndroid` method represents
+  * the duration of the network request in milliseconds. It indicates the time taken for the request to
+  * complete from the moment it was initiated until the response was received. This parameter helps in
+  * measuring the performance of network requests and identifying any potential
+  * @param requestHeaders requestHeaders is a string parameter that contains the headers of the network
+  * request. It typically includes information such as the content type, authorization token, and any
+  * other headers that were sent with the request.
+  * @param requestBody The `requestBody` parameter in the `networkLogAndroid` method represents the
+  * body of the HTTP request being logged. It contains the data that is sent as part of the request to
+  * the server. This could include form data, JSON payload, XML data, or any other content that is
+  * being transmitted
+  * @param requestBodySize The `requestBodySize` parameter in the `networkLogAndroid` method represents
+  * the size of the request body in bytes. It is a double value that indicates the size of the request
+  * body being sent in the network request. This parameter is used to log information related to the
+  * network request, including details
+  * @param requestMethod The `requestMethod` parameter in the `networkLogAndroid` method represents the
+  * HTTP method used in the network request, such as GET, POST, PUT, DELETE, etc. It indicates the type
+  * of operation that the client is requesting from the server.
+  * @param requestUrl The `requestUrl` parameter in the `networkLogAndroid` method represents the URL
+  * of the network request being logged. It typically contains the address of the server to which the
+  * request is being made, along with any additional path or query parameters required for the request.
+  * This URL is essential for identifying the
+  * @param requestContentType The `requestContentType` parameter in the `networkLogAndroid` method
+  * represents the content type of the request being made. This could be values like
+  * "application/json", "application/xml", "text/plain", etc., indicating the format of the data being
+  * sent in the request body. It helps in specifying
+  * @param responseHeaders The `responseHeaders` parameter in the `networkLogAndroid` method represents
+  * the headers of the response received from a network request. These headers typically include
+  * information such as content type, content length, server information, and any other metadata
+  * related to the response. The `responseHeaders` parameter is expected to
+  * @param responseBody The `responseBody` parameter in the `networkLogAndroid` method represents the
+  * body of the response received from a network request. It contains the data or content sent back by
+  * the server in response to the request made by the client. This could be in various formats such as
+  * JSON, XML, HTML
+  * @param responseBodySize The `responseBodySize` parameter in the `networkLogAndroid` method
+  * represents the size of the response body in bytes. It is a double value that indicates the size of
+  * the response body received from the network request. This parameter is used to log information
+  * related to the network request and response, including
+  * @param statusCode The `statusCode` parameter in the `networkLogAndroid` method represents the HTTP
+  * status code of the network request/response. It indicates the status of the HTTP response, such as
+  * success (200), redirection (3xx), client errors (4xx), or server errors (5xx). This parameter is
+  * @param responseContentType The `responseContentType` parameter in the `networkLogAndroid` method
+  * represents the content type of the response received from the network request. It indicates the
+  * format of the data in the response, such as JSON, XML, HTML, etc. This information is useful for
+  * understanding how to parse and handle the
+  * @param errorDomain The `errorDomain` parameter in the `networkLogAndroid` method is used to specify
+  * the domain of an error, if any occurred during the network request. If there was no error, this
+  * parameter will be `null`.
+  * @param w3cAttributes The `w3cAttributes` parameter in the `networkLogAndroid` method is a
+  * ReadableMap object that contains additional attributes related to W3C external trace. It may
+  * include the following key-value pairs:
+  * @param gqlQueryName The `gqlQueryName` parameter in the `networkLogAndroid` method represents the
+  * name of the GraphQL query being executed. It is a nullable parameter, meaning it can be null if no
+  * GraphQL query name is provided. This parameter is used to log information related to GraphQL
+  * queries in the network logging
+  * @param serverErrorMessage The `serverErrorMessage` parameter in the `networkLogAndroid` method is
+  * used to pass any error message received from the server during network communication. This message
+  * can provide additional details about any errors that occurred on the server side, helping in
+  * debugging and troubleshooting network-related issues.
+  */
     @ReactMethod
     private void networkLogAndroid(final double requestStartTime,
                                    final double requestDuration,
