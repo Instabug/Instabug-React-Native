40ea7e35c0178501322951ba2d4b1354b6f5901d
Mohamed Kamal <m.kamalgamil@gmail.com>
2025-03-09 04:05:45 +0200

    fix: remove extra space between 'v' and sdk version number

--- scripts/releases/get_sdk_version.sh
+++ scripts/releases/get_sdk_version.sh
@@ -1,5 +1,5 @@
 sdk_version=$(grep -i 'version' package.json) #"version": "xx.x.x+x",
-sdk_version=$(cut -f2 -d':' <<< "$sdk_version" | tr -d '"' | tr -d ",") #xx.x.x+x,
-sdk_version=$(cut -f1 -d'+' <<< "$sdk_version") #xx.x.x
+sdk_version=$(cut -f2 -d' ' <<< $sdk_version | tr -d '" ,') #xx.x.x+x,
+sdk_version=$(cut -f1 -d'+' <<< $sdk_version) #xx.x.x
 
 echo "$sdk_version"
