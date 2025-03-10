139e6ab316eea256ac086b4e5779943f73007736
Mohamed Kamal <m.kamalgamil@gmail.com>
2025-03-10 16:42:12 +0200

    chore: remove commented code from shell scripts

--- scripts/releases/get_job_approver.sh
+++ scripts/releases/get_job_approver.sh
@@ -1,16 +1,13 @@
 jobsJson=$(curl -s -X GET "https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job" --header "Circle-Token: $CIRCLE_TOKEN")
 
-#jobsJson=$(echo "$jobsJson" | jq -R '.' | jq -s '.' | jq -r 'join("")')
 job=$(jq '.items[] | select(.name == "hold_release_slack_notification")' <<< "$jobsJson")
 
-#job=$(echo "$job" | jq -R '.' | jq -s '.' | jq -r 'join("")')
 approver_id=$(jq '.approved_by' <<< "$job")
 
 approver_id=$(tr -d '"' <<< "$approver_id")
 
 user=$(curl -s -X GET "https://circleci.com/api/v2/user/$approver_id" --header "Circle-Token: $CIRCLE_TOKEN")
 
-#user=$(echo "$user" | jq -R '.' | jq -s '.' | jq -r 'join("")')
 username=$(jq '.login' <<< "$user")
 
 username=$(tr -d '"' <<< "$username")
