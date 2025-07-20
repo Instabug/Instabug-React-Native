6be5bf228d2c0bcd1bed4ba81e21aed0f443a4c8
Ahmed alaa <aalaa@instabug.com>
2025-07-20 15:21:32 +0300

    snapshot

--- examples/default/src/components/Select.tsx
+++ examples/default/src/components/Select.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+
+import { CheckIcon, Select as NativeBaseSelect } from 'native-base';
 
 interface SelectItem<T> {
   label: string;
@@ -14,47 +14,29 @@ interface SelectProps<T> {
   onValueChange: (value: T) => void;
 }
 
-export function Select<T extends string | number>({ label, items, onValueChange }: SelectProps<T>) {
+export function Select<T>({ label, items, onValueChange }: SelectProps<T>) {
   const initialItem = items.find((i) => i.isInitial) ?? items[0];
-  const [selectedValue, setSelectedValue] = useState<T>(initialItem.value);
+  const [selectedItem, setSelectedItem] = useState(initialItem);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>{label}</Text>
-      <View style={styles.pickerWrapper}>
-        <Picker
-          selectedValue={selectedValue}
-          mode={'dropdown'}
-          onValueChange={(value) => {
-            setSelectedValue(value);
-            onValueChange(value);
-          }}
-          style={styles.picker}>
-          {items.map((item) => (
-            <Picker.Item key={item.label} label={item.label} value={item.value} />
-          ))}
-        </Picker>
-      </View>
-    </View>
+    <NativeBaseSelect
+      height="10"
+      placeholder={label}
+      accessibilityLabel={label}
+      selectedValue={selectedItem.label}
+      onValueChange={(value) => {
+        const item = items.find((i) => i.label === value)!;
+        setSelectedItem(item);
+        onValueChange(item.value);
+      }}
+      _selectedItem={{
+        bg: 'coolGray.200',
+        rounded: '10',
+        endIcon: <CheckIcon size="4" />,
+      }}>
+      {items.map((item) => (
+        <NativeBaseSelect.Item key={item.label} label={item.label} value={item.label} />
+      ))}
+    </NativeBaseSelect>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginVertical: 10,
-  },
-  label: {
-    marginBottom: 4,
-    fontSize: 14,
-    color: '#555',
-  },
-  pickerWrapper: {
-    borderWidth: 1,
-    borderRadius: 8,
-    borderColor: '#ccc',
-    overflow: 'hidden',
-  },
-  picker: {
-    height: 44,
-  },
-});
