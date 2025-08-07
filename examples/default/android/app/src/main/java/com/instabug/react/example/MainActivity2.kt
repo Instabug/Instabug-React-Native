6adb2ff7f1296768ffcd8a6b91ab5c44fd2054f7
ahmed alaa <154802748+ahmedAlaaInstabug@users.noreply.github.com>
2025-08-07 15:35:06 +0300

    Release: v15.0.2

--- examples/default/android/app/src/main/java/com/instabug/react/example/MainActivity2.kt
+++ examples/default/android/app/src/main/java/com/instabug/react/example/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.instabug.react.example
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,10 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
